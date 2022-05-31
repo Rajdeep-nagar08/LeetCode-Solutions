aa42b5123cb8b147ff00b8694ad55b6ae8385728
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-06-01 00:14:20 +0530

    Time: 714 ms (36.31%), Space: 26.4 MB (46.50%) - LeetHub

--- 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
@@ -1,4 +1,23 @@
-int dp[18][1<<19];
+/*
+
+for each  nums[i] we have choices among n slots
+
+to store the condition of slots we use mask
+
+two adjacent bits represent the situation of slot
+
+00 00 00 11 01 => it means 1st slot 1 integer , 2nd slot have 2 integers 
+                  and other slots are empty
+
+as max no of slots =9
+so size of bitmask=18
+
+dp[i][mask] = max And sum using [i...n-1] array if condition of slots till now is stored in mask
+
+*/
+
+
+int dp[18][1<<18];
 class Solution {
 public:
     int maximumANDSum(vector<int>& nums, int slots) {
