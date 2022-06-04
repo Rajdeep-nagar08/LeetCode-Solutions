dbe64a71109e6a2aa2e746b953d2ce056d389ee5
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-06-04 12:02:49 +0530

    Time: 1680 ms (5.88%), Space: 317.4 MB (5.88%) - LeetHub

--- 1001-grid-illumination/1001-grid-illumination.cpp
+++ 1001-grid-illumination/1001-grid-illumination.cpp
@@ -1,9 +1,4 @@
-int dx[9]={1,-1,0,0,-1,-1,1,1,0};
-int dy[9]={0,0,1,-1,-1,1,-1,1,0};
-class Solution {
-public:
-    vector<int> gridIllumination(int n, vector<vector<int>>& lmp, vector<vector<int>>& q) {
-        
+
         /*
         
         no of queries = 2*10^4
@@ -12,9 +7,23 @@ public:
         
         so it is not possible to make the complete grid
         
+        store the information of each bulb we are going to lamped using unordered maps
+        
         */
         
-        unordered_map<int,unordered_map<int,int>>r,c,dg1,dg2;
+
+
+
+class Solution {
+public:
+
+int dx[9]={1,-1,0,0,-1,-1,1,1,0};
+int dy[9]={0,0,1,-1,-1,1,-1,1,0};
+    
+    vector<int> gridIllumination(int n, vector<vector<int>>& lmp, vector<vector<int>>& q) {
+        
+        map<int,map<int,int>>r,c,dg1,dg2;
+        
         // r => columns
         // c => rows
         // r-c => columns
@@ -25,10 +34,10 @@ public:
         int sz=lmp.size();
         
         for(int i=0;i<sz;i++){
-            r[lmp[i][0]][lmp[i][1]]++;
-            c[lmp[i][1]][lmp[i][0]]++;
-            dg1[lmp[i][0]-lmp[i][1]][lmp[i][1]]++;
-            dg2[lmp[i][0]+lmp[i][1]][lmp[i][1]]++;
+            r[lmp[i][0]][lmp[i][1]]++;  // [r=>columns]
+            c[lmp[i][1]][lmp[i][0]]++;  // [c=> rows]
+            dg1[lmp[i][0]-lmp[i][1]][lmp[i][1]]++; // [d1,y]
+            dg2[lmp[i][0]+lmp[i][1]][lmp[i][1]]++; // [d2,y]
         }
         
         int sz1=q.size();
@@ -36,32 +45,54 @@ public:
         
         int r1,c1,d1,d2;
         
+        int x,y;
+        
         for(int i=0;i<sz1;i++){
+            
             r1=q[i][0];
             c1=q[i][1];
             d1=r1-c1;
             d2=r1+c1;
- if(r[r1].size()>0 || c[c1].size()>0 || dg1[d1].size()>0 || dg2[d2].size()>0 )
+            
+ if((int)r[r1].size()>0 || (int)c[c1].size()>0 || (int)dg1[d1].size()>0 || (int)dg2[d2].size()>0 )
                 ans[i]=1;
            
             
             for(int j=0;j<9;j++){
-                int x=r1+dx[j];
-                int y=c1+dy[j];
+                x=r1+dx[j];
+                y=c1+dy[j];
                 if(x>=0 && y>=0 && x<n && y<n){
-                  // erase [r,c]
-                  // erase [c,r]
-                  // erase [r-c,c]
+                    
                     r[x].erase(y);
                     c[y].erase(x);
                     dg1[x-y].erase(y);
                     dg2[x+y].erase(y);
+                    
+                
+                    /*
+                    auto it1=r[x].find(y);
+                    if(it1!=r[x].end())
+                        r[x].erase(it1);
+                    
+                    auto it2=c[y].find(x);
+                    if(it1!=c[y].end())
+                        c[y].erase(it2);
+                    
+                    auto it3=dg1[x-y].find(y);
+                    if(it3!=dg1[x-y].end())
+                        dg1[x-y].erase(it3);
+                    
+                    auto it4=dg2[x+y].find(y);
+                    if(it4!=dg2[x+y].end())
+                        dg2[x+y].erase(it4);
+                        
+                        */
+                    
                 }
               }
                 
             }
         
-       // cout<<d[0]<<endl;
         
         return ans;
     }
