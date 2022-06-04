c7e0da37a660f6c68debb667efb73d695c6170e6
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-06-04 09:56:18 +0530

    Time: 24 ms (28.55%), Space: 12.1 MB (23.87%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<vector<string>>ans;
+    
+    vector<vector<string>> solveNQueens(int n) {
+        
+        vector<string>board(n);
+        
+        string str="";
+        
+        for(int i=0;i<n;i++)
+            str+=".";
+        
+        for(int i=0;i<n;i++)
+            board[i]=str;
+        
+        find(0,n,board);
+        
+        return ans;
+    }
+    
+      void find(int r,int n,vector<string>board){
+          
+          if(r==n){
+              ans.push_back(board);
+              return ;
+          }
+          
+          for(int c=0;c<n;c++){
+              if(isValid(r,c,board,n)){
+                  board[r][c]='Q';
+                  
+                  find(r+1,n,board);
+                    //  return true;
+                  
+                  board[r][c]='.';
+
+              }
+             // return false;
+          }
+         // return false;
+      }
+    
+  
+    bool isValid(int r,int c,vector<string>&board,int n){
+        for(int i=0;i<r;i++){
+          for(int j=0;j<n;j++){
+              if(board[i][j]=='Q'){
+            if(i==r || j==c || (i-j)==(r-c) || (j-i)==(c-r) || (i+j)==(r+c))
+                      return false;
+              }
+          }
+        }
+        return true;
+    }
+   
+};
