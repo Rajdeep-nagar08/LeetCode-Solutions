6cf166558a3a6efb2b242a71f1f99044c63d3632
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-06-08 17:35:51 +0530

    Time: 246 ms (43.39%), Space: 76 MB (34.71%) - LeetHub

--- 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
+++ 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
@@ -1,79 +1,61 @@
-    /*
-        
-        remove all the cars containing illegal goods, to remove cars we have
-        3 choices
-        
-        We needs to find the minimum time to remove all cars having illigal goods
-        
-        Note: its not necessary to remove only illigal goods cars, we can remove any car but finally there should not be any car that have illigal goods
-        
-        
-        => remove from front : time =1
-        => remove from last : time=1
-        => remove from middle : time=2
-        
-        
-  
-  if front is 1 , remove it
-  if front is 0 , than two choices :
-          remove 0
-          remove 1 that is next to 0 if that 1 is not in last
-  
-  if back is 1 , remove it
-  if back is 0, than two choices :
-           remove 0
-           remove 1 that is prev to 0
-           
-  */
-
-
 /*
 
-The optimal strategy will be:
+using left and right array
 
-Take some elements from the left
-Take some elements from the right
-Take the rest elements from the middle.
-Now, the question is how to choose left and right parts, we can have potentially O(n^2) options and we can not check them all. Let us calculate how much we paid:
+left[i]= min cost to remove all 1's from 0 to i 
 
-|..left..|..middle..|..right..|
+if(s[i]=='1')
+  left[i] = min(left[i-1]+2(cost to remove it directly),i+1 (cost to remove all from 0 to i to remove i))
+  
+  
+ similarly
 
-For left and right we paid just their lengths. For middle we pay twice number of ones se have inside, so we have: len(left) + 2* count(middle, 1) + len(right) = len(left) + len(middle) + len(right) + 2*count(middle, 1) - len(middle) = n + count(middle, 1) - count(middle, 0).
+right[i]= min cost to remove s[i] if s[i]=='1' from right side
 
-So, in fact what we need to found is the subarray with the smallest count(middle, 1) - count(middle, 0) value. 
-If we now replace all 0 with -1, it is the same as found the subarray with the smallest sum! 
-
-Ans= n + (minimum sum subarray after replacing all 0 by -1)
-
-So use Standard Kedan's algorithm to find this
-
-*/
-      
+if(s[i]=='1')
+  right[i]= min(right[i+1]+2 , n-i)
+  
+  Ans = min(left[i],right[i+1]) for all i
+  
+  */
 
 class Solution {
 public:
-    int minimumTime(string str) {
-
-        int n=str.size();
-       
-        int mn=INT_MAX;
+    int minimumTime(string s) {
         
-        int temp=0;
         
-        int no;
+        int n=s.size();
         
-        for(int i=0;i<str.size();i++){
-            if(str[i]=='0')
-                no=-1;
+        vector<int>left(n);
+        
+        left[0]=s[0]=='1'?1:0;
+        
+        for(int i=1;i<n;i++){
+            if(s[i]=='1')
+                left[i]=min(left[i-1]+2,i+1);
             else
-                no=1;
-            
-            temp=min(temp+no,no);
-            mn=min(mn,temp);
+                left[i]=left[i-1];
         }
         
-      return min(n+mn,n);
-       
+         vector<int>right(n);
+        
+           right[n-1]=s[n-1]=='1'?1:0;
+        
+        for(int i=n-2;i>=0;i--){
+            if(s[i]=='1')
+                right[i]=min(right[i+1]+2,n-i);
+            else
+                right[i]=right[i+1];
+        }
+        
+        int ans=min(left[n-1],right[0]);
+        
+         for(int i=0;i<=n-2;i++){
+             
+          ans=min(ans,left[i]+right[i+1]);   
+      }
+
+        
+        return ans;
     }
-    
 };
