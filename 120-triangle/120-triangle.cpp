41b0191b643b8d7b1c8942e2619b1ef557900088
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-06-13 09:32:52 +0530

    Time: 15 ms (14.49%), Space: 12.4 MB (5.41%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,31 @@
+int dp[1000][1000]; // ith row and jth column 
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& tr) {
+        
+        memset(dp,-1,sizeof(dp));
+        
+        if(tr.size()==1)
+            return tr[0][0];
+        
+        return tr[0][0]+ min(find(1,0,tr),find(1,1,tr));
+        
+    }
+    
+    int find(int r,int c,vector<vector<int>>&tr){
+        
+        if(r==tr.size()-1)
+            return tr[r][c];
+        
+        if(dp[r][c]!=-1)
+            return dp[r][c];
+        
+        int mn1=INT_MAX,mn2=INT_MAX;
+        
+        mn1=min(mn1,tr[r][c]+find(r+1,c,tr));
+        
+        mn2=min(mn2,tr[r][c]+find(r+1,c+1,tr));
+        
+        return dp[r][c]=min(mn1,mn2);
+    }
+};
