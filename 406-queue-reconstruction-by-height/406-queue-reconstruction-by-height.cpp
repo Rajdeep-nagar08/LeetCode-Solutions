78e20d47fad4563d05a21689481cc6502ea8a0f7
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-06-13 23:21:35 +0530

    Time: 139 ms (67.22%), Space: 11.7 MB (96.95%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -15,6 +15,12 @@
         
         Than position of second shortest person = its k
         
+Pick out tallest group of people and sort them in a subarray (S). Since there's no other groups of people taller than them, therefore each guy's index will be just as same as his k value.
+
+For 2nd tallest group (and the rest), insert each one of them into (S) by k value. So on and so forth.
+
+
+        
 input: [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
 sort: [[7,0], [7,1], [6,1], [5,0], [5,2], [4,4]]
 step1: [[7,0]]
@@ -41,6 +47,29 @@ public:
             else
                 return false;
         });
+        
+        
+        // this can be done by insert function of vector
+        /*
+        
+std::vector::insert() is a built-in function in C++ STL which inserts new elements before the element at the specified position, effectively increasing the container size by the number of elements inserted.
+
+Time Complexity – Linear O(N)
+
+
+        vector vec = { 10, 20, 30, 40 };
+
+// inserts 3 at front
+auto it = vec.insert(vec.begin(), 3);
+// inserts 2 at front
+vec.insert(it, 2);
+
+int i = 2;
+// inserts 7 at i-th index
+it = vec.insert(vec.begin() + i, 7);
+
+
+*/
         
              
              for(int i=1;i<n;i++){
