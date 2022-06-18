3cacfc3dbded66a8852f3cafe70a1ec0d4f8a56e
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-06-18 14:37:47 +0530

    Time: 75 ms (5.05%), Space: 31.5 MB (5.13%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 
 /*
 
@@ -16,31 +5,203 @@ for each node we have choices either to install camera at this node or not
 
 so we can think about dp
 
-dp[node][parent]= min no of cameras needs for subtree of node if we have two choices for this node (itself), consediring whether parent of u has camera or not
-
+dp[node][parent]= min no of cameras needs for subtree of node if we have two choices for this node (itself), consediring whether parent of node has camera or not
 
 */
+
+
+/*
+
+Recursive ---- TLEEEEEEEEEEEEE
+
 class Solution {
 public:
-    #define NO_CAMERA       0
-    #define HAS_CAMERA      2
-    #define NOT_NEEDED      1
-    int ans = 0;
-    int dfs(TreeNode *root) {
-        if (!root) return NOT_NEEDED;
-        int l = dfs(root->left);
-        int r = dfs(root->right);
-        if (l == NO_CAMERA || r == NO_CAMERA) {
-            ans++;
-            return HAS_CAMERA;
-        } else if (l == HAS_CAMERA || r == HAS_CAMERA) {
-            return NOT_NEEDED;
-        } else {
-            return NO_CAMERA;
+    
+    int minCameraCover(TreeNode* root) {
+       
+        int c1=INT_MAX,c2=INT_MAX,c3=INT_MAX;
+
+        // giving camera to root
+        
+        c1=min(c1,1+find(root->left,1)+find(root->right,1));
+        
+        // not giving camera to root, then there must be atleast one camera on its any child
+        
+        if(root->left && root->right){
+        
+           c2= min(c2,1+ find(root->left->left,1)+find(root->left->right,1)+find(root->right,0));
+          
+            c3= min(c3,1+ find(root->left,0)+ find(root->right->left,1)+find(root->right->right,1));
+            
+            
+        }
+        
+        else if(root->left){
+    c2= min(c2,1+ find(root->left->left,1)+find(root->left->right,1));
+          
+            
+        }
+        
+        else if(root->right){
+    c2= min(c2,1+find(root->right->left,1)+find(root->right->right,1));
+             
+        }
+         
+        return min({c1,c2,c3});
+        
+    }
+    
+    
+   int find(TreeNode* root,int par){
+        
+        if(!root)
+            return 0;
+        
+        int c1=INT_MAX,c2=INT_MAX,c3=INT_MAX;
+        
+        if(par==1){
+            
+         // either camera to this root or not give camera
+        
+            c1=min(c1,1+find(root->left,1)+find(root->right,1));
+               
+           c2=min(c2,find(root->left,0)+find(root->right,0));
+
+           
+        }
+        
+        else{
+            
+           // giving camera to root
+        
+        c1=min(c1,1+find(root->left,1)+find(root->right,1));
+        
+        // not giving camera to root, then there must be atleast one camera on its any child
+        
+        if(root->left && root->right){
+        // giving to left not to right
+  c2= min(c2,1+ find(root->left->left,1)+find(root->left->right,1)+
+                 find(root->right,0));
+        // not giving to right, giving to left  
+ c3= min(c3,1+ find(root->left,0)+
+                 find(root->right->left,1)+find(root->right->right,1));
+              
+        }
+            
+        else if(root->left){
+    c2= min(c2,1+ find(root->left->left,1)+find(root->left->right,1));
+          
+            
+        }
+            
+        else if(root->right){
+    c2= min(c2,1+find(root->right->left,1)+find(root->right->right,1));
+             
         }
     }
-    int minCameraCover(TreeNode* root) {
-        if (dfs(root) == NO_CAMERA) ans++;
-        return ans;
-    }
+        
+    return min({c1,c2,c3});
+        
+}
+
 };
+
+*/
+
+unordered_map<TreeNode*,unordered_map<int,int>>mp;
+
+class Solution {
+public:
+    
+    int minCameraCover(TreeNode* root) {
+       
+        int c1=INT_MAX,c2=INT_MAX,c3=INT_MAX;
+
+        // giving camera to root
+        
+        c1=min(c1,1+find(root->left,1)+find(root->right,1));
+        
+        // not giving camera to root, then there must be atleast one camera on its any child
+        
+        if(root->left && root->right){
+        
+           c2= min(c2,1+ find(root->left->left,1)+find(root->left->right,1)+find(root->right,0));
+          
+            c3= min(c3,1+ find(root->left,0)+ find(root->right->left,1)+find(root->right->right,1));
+            
+            
+        }
+        
+        else if(root->left){
+    c2= min(c2,1+ find(root->left->left,1)+find(root->left->right,1));
+          
+            
+        }
+        
+        else if(root->right){
+    c2= min(c2,1+find(root->right->left,1)+find(root->right->right,1));
+             
+        }
+         
+        return min({c1,c2,c3});
+        
+    }
+    
+    
+   int find(TreeNode* root,int par){
+        
+        if(!root)
+            return 0;
+       
+       if(mp[root].count(par))
+           return mp[root][par];
+        
+        int c1=INT_MAX,c2=INT_MAX,c3=INT_MAX;
+        
+        if(par==1){
+            
+         // either camera to this root or not give camera
+        
+            c1=min(c1,1+find(root->left,1)+find(root->right,1));
+               
+           c2=min(c2,find(root->left,0)+find(root->right,0));
+
+           
+        }
+        
+        else{
+            
+           // giving camera to root
+        
+        c1=min(c1,1+find(root->left,1)+find(root->right,1));
+        
+        // not giving camera to root, then there must be atleast one camera on its any child
+        
+        if(root->left && root->right){
+        // giving to left not to right
+  c2= min(c2,1+ find(root->left->left,1)+find(root->left->right,1)+
+                 find(root->right,0));
+        // not giving to right, giving to left  
+ c3= min(c3,1+ find(root->left,0)+
+                 find(root->right->left,1)+find(root->right->right,1));
+              
+        }
+            
+        else if(root->left){
+    c2= min(c2,1+ find(root->left->left,1)+find(root->left->right,1));
+          
+            
+        }
+            
+        else if(root->right){
+    c2= min(c2,1+find(root->right->left,1)+find(root->right->right,1));
+             
+        }
+    }
+        
+    return mp[root][par]= min({c1,c2,c3});
+        
+}
+
+};
+
