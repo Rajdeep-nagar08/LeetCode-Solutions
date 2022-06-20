d5c88fcee38d7bcc20a0652fcdb3029cf3e8eb88
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-06-20 14:23:59 +0530

    Time: 6 ms (85.15%), Space: 7.4 MB (29.33%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.cpp
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -1,35 +1,93 @@
+
+
 class Solution {
 public:
 
     int repeatedStringMatch(string a, string b) {
-    
+        
         int n1=a.size();
         
         int n2=b.size();
         
-        int i=0,j=0;
+        int need=n2/n1;
         
-        int count=1;
+        string s="";
         
-        string a1=a;
-        
-        while(i<n1){
-            int idx=i+n2;
-            
-            if(i+n2-1<a1.size()){
-                string s=a1.substr(i,n2);
-                if(s==b)
-                    return count;
-                
-                i++;
-            }
-            else{
-                a1=a1+a;
-                count++;
-            }
+        for(int i=0;i<=need+1;i++){
+            s+=a;
         }
+        
+       int idx=search(s,b);
+        
+     //   cout<<s<<" "<<idx<<endl;
+        
+        if(idx==-1)
+            return -1;
+        
+        int sz=s.size();
+        
+        int len=idx+n2;
+        
+        return (len/n1+(len%n1!=0));
+   
+        
+    }
     
-        return -1;
+
+    int search(string &text,string &pat){
+
+    vector<int>lps(pat.length(),0);
+           
+    makelps(pat,lps);
+
+    int i=0,j=0;
+
+    while(i<text.length()){
+
+      if(text[i]==pat[j]){
+      i++;
+      j++;
+      }
+
+      else{
+        if(j!=0)
+        j=lps[j-1];
+        else
+        i++;
+      }
+
+      if(j==pat.length()){
+          return i-j;
+      j=lps[j-1];
+       }
+
+    }
+        
+    return -1;
   }
+    
+    
+    void makelps(string pat,vector<int>&lps) {
+      int i=1,j=0;
+      lps[0]=0;
+      while(i<pat.length()){
+        if(pat[i]==pat[j]){
+          lps[i]=j+1;
+          j++;
+          i++;
+        }
+        else{
+          if(j!=0)
+            j=lps[j-1];
+          else{
+            lps[i]=0;
+            i++;
+          }
+        }
+      }
+      return;
+    }
+
 };
+
 
