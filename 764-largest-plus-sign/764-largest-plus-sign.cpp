0571fbc7c8fa38253833c56515854c887ac1b0fb
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-07-08 01:48:40 +0530

    Time: 255 ms (87.07%), Space: 33.4 MB (75.28%) - LeetHub

--- 764-largest-plus-sign/764-largest-plus-sign.cpp
+++ 764-largest-plus-sign/764-largest-plus-sign.cpp
@@ -12,7 +12,33 @@ public:
       
         int mx=0;
                 
-        for(int i=0;i<n;i++){
+        for(int i=n/2;i<n;i++){
+            for(int j=0;j<n;j++){
+                
+                int k=0;
+                
+                int mnl=min({i,j,n-1-i,n-1-j});
+                
+                if(mnl+1<=mx)
+                    continue;
+                
+                while(i+k<n && i-k>=0 && j+k<n && j-k>=0){
+                    
+    if(v[i][j+k]!=1 || v[i][j-k]!=1 || v[i+k][j]!=1 || v[i-k][j]!=1)
+                        break;
+                    
+                    k++;
+                }
+                
+            
+                mx=max(mx,k);
+                
+                
+            }
+        }
+        
+        
+         for(int i=0;i<=n/2;i++){
             for(int j=0;j<n;j++){
                 
                 int k=0;
