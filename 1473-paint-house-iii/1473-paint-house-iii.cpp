aa8690a41868078dff3f26fe36081366a9cf02e1
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-07-08 14:35:43 +0530

    Time: 66 ms (94.95%), Space: 10.6 MB (92.29%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -4,31 +4,31 @@ No of components formed after painting each remaining house = target
 
 Cost of painting should be minimum
     
-        among all the colors for a particular house, we have a choice what color to choose or not, so we can think about dp
+        among all the colors for a particular uncolored house, we have a choice what color to choose or not for it, As we have choices so we can think about dp
         
-        We needs to break our array into 'target' parts such that color of each part should be same [and adjacent parts should have differet colors]
+        We needs to break our array into 'target' components such that color of each component should be same [and adjacent component should have differet colors]
         
  And cost to give colors to uncolored houses should be minimum
         
- 
- For each uncolored house we have only one choice if that house lies in a
- component where some house already have some color (give that color to all houses of that component)
- 
- If there is no house already colored in current component:
-    1. If component is started or last component than no of choices of colors = n-1 => e.g  [........(color = 1)(any color except 1)]
+Now there can be two cases:
+
+1. if current house is colored than:
+     1. if its color is similar to previous house color than include this house with previous house
+     2. if its color is different from previous house color than start new component from here
     
-    2. If component is middle one, than no of choices of colors = n-2
-     e.g [....(color=1)(any color except 1 and 2)(color=2)]
+
+2. if current house is not colored, than try to give all colors to it:
+
+    1. if color givento it is similar to previous house color than include this house with previous house
+   
+    2. If color given to it is different from previous house color than start new component from here
      
-    
-    
     
 dp[i][target][prev] = min cost to color houses [i.....m-1] 
 
- no of components needs to form = target , color of prev component = prev
+ , No. of components needs to form = target , color of prev component = prev
 
 Ans = dp[0][target][0]
-
 
 
 */
@@ -42,33 +42,7 @@ public:
     int minCost(vector<int>& h, vector<vector<int>>& cost, int m, int n, int target) {
         
         
-        // no of houses =m
-        
-        int count=0;
-        
-        int i=0;
-        
-        while(i<m){
-            if(h[i]==0){
-                i++;
-                continue;
-            }
-            else{
-                int col=h[i];
-                count++;
-                while((i<m) && (col==h[i])){
-                    i++;
-                }
-            }
-        }
-        
-        if(m==100 && target==100)
-            return 1000000;       
-        
-        /*
-        if(count>target)
-            return -1;
-             */
+        // no of houses =m   
         
         memset(dp,-1,sizeof(dp));
         
@@ -82,10 +56,10 @@ public:
     }
     
     
-    
     int find(int i,int target,int prev,vector<int>&h,vector<vector<int>>&cost,int m,int n){
         
         if(i>=m){
+            
             if(target==0){
                 return 0;
             }
@@ -94,104 +68,44 @@ public:
         }
         
         
-        if(target==0)
+        if(target<0)
             return 1e8;
+        
         
         if(dp[i][target][prev]!=-1)
             return dp[i][target][prev];
         
         
-        int mn=1e8;
         
+    int mn1=1e8,mn2=1e8;    
         
-        for(int sz=1;sz<=m-target+1;sz++){
+        // case 1 (house already colored)
             
-            // size of current component = sz
-            
-            int j;
-            
-            if(target==1){
-                sz=m;
-                j=m-1;
-            }
-            
-            else
-              j=i+sz-1;
-            
-            // component = [i....j]
-            
-            if(j>=m)
-                break;
-                        
-            bool flag=true;
-            
-            int col=0;
-            
-            for(int i1=i;i1<=j;i1++){
-                if(h[i1]!=0){
-                    if(col==0){
-                        col=h[i1];
-                    }
-                    else if(col!=h[i1]){
-                        flag=false;
-                        break;
-                    }
-                }
-            }
-           
- // two colors in [i,j]
-            if(flag==false)
-                break;
-            
-// color of [i,j] is similar to prev component
-            if(prev!=0 && (prev==col))  
-                break;
-                        
-            if(col==0){ 
-                
-// give any color that is not similar to prev one , to all uncolored house in [i,j1]
-                
-            for(int c=1;c<=n;c++){
-                
-                if(c==prev)
-                    continue;
-                
-                int ct=0;  // cost to give color 'c'
-
-                for(int i2=i;i2<=j;i2++){
-                    if(h[i2]==0)
-                    ct+=cost[i2][c-1];
-                }
-                
-                mn=min(mn,ct+find(j+1,target-1,c,h,cost,m,n));
-                
-            }
-                
-                
-       }
-            
-            else{  //give 'col' to all uncolored houses in [i,j1]
-                
-               int ct=0;
-                
-               int c=col;
-                
-                 for(int i2=i;i2<=j;i2++){
-                    if(h[i2]==0)
-                    ct+=cost[i2][c-1];
-                }
-                
-            
-                mn=min(mn,ct+find(j+1,target-1,c,h,cost,m,n));
-  
-                 
-            }
-            
+        if(h[i]!=0){
+            if(h[i]!=prev)   // new component starts from here
+          mn1=min(mn1,find(i+1,target-1,h[i],h,cost,m,n));
+            else             // previous component going on
+          mn1=min(mn1,find(i+1,target,h[i],h,cost,m,n));
         }
         
+        // case 2 (house not colored, try all colorss for it)
         
-        return dp[i][target][prev]=mn;
+        else{
+          
+       for(int c=1;c<=n;c++){
+           if(prev!=c)   // new component starts from here
+        mn2=min(mn2,cost[i][c-1]+find(i+1,target-1,c,h,cost,m,n));
+           else          // previous component going on
+        mn2=min(mn2,cost[i][c-1]+find(i+1,target,c,h,cost,m,n));
+      
+       }
+            
+    }
         
-        
+        return dp[i][target][prev]=min(mn1,mn2);
+          
     }
 };
+
+
+//T.C = O(m*m*n)*O(n) = O(m*m*n*n)
