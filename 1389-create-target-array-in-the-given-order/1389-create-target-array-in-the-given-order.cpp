e72a903aa490451f8d13e83a3ddd6ea2a8551f96
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-07-13 12:09:12 +0530

    Time: 7 ms (21.00%), Space: 8.6 MB (10.12%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        
+        
+        vector<int>target;
+        
+        int n=index.size();
+        
+        for(int i=0;i<n;i++){
+            target.insert(target.begin()+index[i],nums[i]);
+        }
+        
+        return target;
+    }
+};
