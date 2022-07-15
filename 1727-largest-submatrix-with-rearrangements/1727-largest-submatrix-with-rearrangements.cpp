32bd9282ec32d87399d1e821e216c937ca471430
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-07-15 11:59:49 +0530

    Time: 302 ms (56.79%), Space: 66.7 MB (67.90%) - LeetHub

---
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& grid) {
+        
+        int n=grid.size();
+    
+        int m=grid[0].size();
+        
+        int mx=0;
+        
+     // For each column, finding the number of consecutive ones ending at each position.
+                
+        for(int j=0;j<m;j++){
+            
+           int i=0;
+            
+            while(i<n){
+                
+           if(grid[i][j]==0 || i==0){
+               i++;
+               continue;
+           }
+            
+          while(i<n && (grid[i][j]==1)){
+              grid[i][j]+=grid[i-1][j];
+              i++;
+             }
+        
+            }
+           
+        }
+        
+        
+       vector<int>v(m);
+        for(int i=0;i<n;i++){
+            
+            
+            for(int j=0;j<m;j++){
+                v[j]=grid[i][j];
+            }
+            
+            sort(v.rbegin(),v.rend());
+            
+            int area=0;
+            
+            for(int j=0;j<m;j++){
+                int ht=v[j];
+                int wt=(j+1);
+                area=max(area,ht*wt);
+            }
+            
+            mx=max(mx,area);
+            
+        }
+        
+     
+        return mx;
+    }
+};
