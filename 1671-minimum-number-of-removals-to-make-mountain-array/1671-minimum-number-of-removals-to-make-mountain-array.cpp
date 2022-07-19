39cb9c6f1485708b6f238b49238bd43a7efed0c7
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-07-19 16:04:27 +0530

    Time: 529 ms (21.94%), Space: 12.1 MB (59.07%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& arr) {
+        
+        int n=arr.size();
+        
+        /*
+        
+        [increasing.......(peak)....decreasing]
+        
+        Ans:
+        [LIS(length=l1)....(peak).....LDS(length=l2)]=mountain array of length l1+1+l2
+        
+        minimum removals = n-(l1+l2+1);
+        
+        Lis[i]=length of longest incresing subsequence ends at index i
+        Lds[i]=length of longerst decreasing subsequence ends at index i
+        
+        so for each index i=1,n-1 Calculate LIS on left of i
+        
+        and for each i=n-2,0 Calculated LDS on rigth of i
+        
+        than for each index i
+        ans=max(ans,n-(LIS[i]+LDS[i]+1))    
+        
+        */
+        
+        vector<int>LIS(n,1);
+        vector<int>LDS(n,1);
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i])
+                   LIS[i]=max(LIS[i],1+LIS[j]);
+                }
+            }
+            
+             for(int i=n-2;i>=0;i--){
+            for(int j=n-1;j>i;j--){
+                if(arr[j]<arr[i])
+                   LDS[i]=max(LDS[i],1+LDS[j]);
+                }
+            }
+        
+        
+        int ans=1;
+        
+        for(int i=1;i<n-1;i++){
+            int l=LIS[i];
+            int r=LDS[i];
+            if(l>1 && r>1)
+            ans=max(ans,l+r-1);
+        }
+        
+        return n-ans;
+    }
+};
