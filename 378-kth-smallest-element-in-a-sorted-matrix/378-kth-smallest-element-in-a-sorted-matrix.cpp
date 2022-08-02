44f5a1f9a4c15647088baf483aa1722eb5d962ce
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-08-02 18:21:46 +0530

    Time: 33 ms (91.05%), Space: 13.1 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k)
-    {
-        
-        int n=matrix.size();
-        
-        
-        int high=matrix[n-1][n-1];
-        
-        int low=matrix[0][0];
-        
-        while(low<=high)
-        {
-        
-        int mid=(high+low)/2;
-        
-        int count =0;
-        
-        for(int i=0;i<n;i++)
-        {
-
-count+=upper_bound(matrix[i].begin(),matrix[i].end(),mid)- matrix[i].begin();
-            
-            
+    int kthSmallest(vector<vector<int>>& mat, int k) {
+        int ans=0;
+      int n=mat.size();
+      int low=mat[0][0];
+      int high=mat[n-1][n-1];
+      while(low<=high){
+        int mid=(high-(high-low)/2);
+        if(smallerThenMid(mat,mid)>=k){
+          ans=mid;
+          high=mid-1;
         }
-            
-            if(count<k)
-            {
-                low=mid+1;
-            }
-            
-            else
-            {
-                high=mid-1;
-            }
-        
-            
-        }
-        
-        return low;
+        else
+          low=mid+1;
+      }
+      return ans;
     }
+  
+     int smallerThenMid(vector<vector<int>>&mat,int x){
+       int j=mat[0].size()-1;
+       int count=0;
+       for(int i=0;i<mat.size();i++){
+         int idx=upper_bound(mat[i].begin(),mat[i].end(),x)-mat[i].begin();
+         count+=idx;
+       }
+       return count;
+     }
 };
