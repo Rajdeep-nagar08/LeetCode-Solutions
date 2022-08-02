7ae30a5a1513b10b981ed861497e56ef90af76e6
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-08-02 18:01:31 +0530

    Time: 1298 ms (5.01%), Space: 242.3 MB (5.01%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -1,31 +1,88 @@
+#define lln long long int
+
+/*
+
+at each point, we have 3 choices to move
+   1. Move downward 
+      sum+= point[i][j]+points[i+1][j]
+   2. Move left (If previous move was not towards right)
+      sum+=points[i][j] - 1 ,    j-1>=0
+   3. Move right (If previous move was not towards left)
+      sum+=points[i][j] - 1 ,    j+1<m
+      
+      Doing '-1' becouse of column difference
+      
+
+dp[i][j][move] = max sum of points we can get if we start moving from [i,j]
+                 ans our previous move was in
+                 direction => move [0-----> down]
+                                   [1-----> left]
+                                   [2-----> right]
+                                
+Ans= max(dp[0][j][0]) for all j=0 to m-1
+
+
+*/
+
+
 class Solution {
 public:
-    #define ll long long
-    ll n, m;
-    ll INF = 1000000000000ll;
-    vector<vector<vector<ll>>> DP;
-    long long dp(ll i, ll j, int dir, vector<vector<int>>& a) {
-        if (j < 0 || j >= m)
-            return -INF;
-        if (i == n)
+    
+    int n,m;
+    
+    long long maxPoints(vector<vector<int>>& pt) {
+       
+        
+ n=pt.size();
+        
+ m=pt[0].size();
+        
+        
+vector<vector<vector<lln>>>dp(n,vector<vector<lln>>(m,vector<lln>(3,-1)));
+        
+        
+    
+     lln sum=0;
+        
+     for(int j=0;j<m;j++){
+         sum=max(sum,pt[0][j]+find(1,j,0,pt,dp));
+     }
+        
+        return sum;
+
+    }
+    
+    
+    lln find(int i,int j,int move,vector<vector<int>>&pt, vector<vector<vector<lln>>>&dp){
+        
+        
+        if(i==n){
             return 0;
-        if (DP[i][j][dir] != -1)
-            return DP[i][j][dir];
-        ll x = a[i][j];
-        ll ans = x + dp(i + 1, j, 0, a);
-        if (dir != 1)
-            ans = max(ans, -1 + dp(i, j - 1, 2, a));
-        if (dir != 2)
-            ans = max(ans, -1 + dp(i, j + 1, 1, a));
-        return DP[i][j][dir] = ans;
-    }
-    long long maxPoints(vector<vector<int>> &a) {
-        n = a.size(), m = a[0].size();
-        DP.resize(n, vector<vector<ll>>(m, vector<ll>(3, -1)));
-        ll ans = INT_MIN;
-        for (int i = 0; i < m; i++) {
-            ans = max(ans, dp(0, i, 0, a));
         }
-        return ans;
+        
+        if(dp[i][j][move]!=-1)
+            return dp[i][j][move];
+        
+        lln ch1=0,ch2=0,ch3=0;
+        
+        // moving down
+        
+        ch1=pt[i][j]+find(i+1,j,0,pt,dp);
+        
+        // moving left
+        
+        if(move!=2 && j-1>=0)
+         ch2=find(i,j-1,1,pt,dp)-1;
+        
+        // moving right
+        
+        if(move!=1 && j+1<m)
+          ch3=find(i,j+1,2,pt,dp)-1;
+        
+        
+        return dp[i][j][move]=max({ch1,ch2,ch3});
+            
     }
+    
+    
 };
