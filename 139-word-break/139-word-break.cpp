5b884c09e25f4bfd7c6ee97e18dfc5e42f1dbcbe
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-08-06 01:29:40 +0530

    Time: 54 ms (14.23%), Space: 18.1 MB (11.10%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,64 @@
+/*
+
+dp[i] =  Is it possible to  break [i....n-1] into dict
+
+Ans= dp[0]
+
+dp[i] = Is str[i....j] present in dict && Is it possible to break [j+1...n-1]
+
+
+dp[i] = Is str[i....j] present in dict && dp[j+1]
+
+for all j=i to n-1
+
+*/
+
+
+int dp[301];
+
+class Solution {
+public:
+    unordered_map<string,int>mp;
+    int n;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        
+        memset(dp,-1,sizeof(dp));
+        
+        for(string str:wordDict){
+            mp[str]++;
+        }
+        
+        n=s.size();
+        
+        return find(0,s);
+          
+    }
+    
+    bool find(int i,string &str){
+        if(i>=n)
+            return true;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        bool ans=false;
+        
+        for(int j=i;j<n;j++){
+            
+            int l=j-i+1;
+            
+            string s1=str.substr(i,l);
+            
+            
+            if(mp[s1]>0){
+                cout<<s1<<" ";
+                ans|=find(j+1,str);
+            }
+            
+        }
+        
+        return dp[i]=ans;
+        
+    }
+};
