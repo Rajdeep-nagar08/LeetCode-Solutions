ecea72fd35293cc83b930fb4d4ff13b179c10804
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-08-14 22:17:50 +0530

    Time: 24 ms (39.72%), Space: 6.8 MB (9.93%) - LeetHub

---
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -0,0 +1,104 @@
+
+/*
+
+Atleast one repeated digit = n-(all digits distinct)
+
+
+dp[i][tight][nonZero][mask]= count of all numbers till n, such that all
+
+digits in range [i...n-1] should not belongs to mask,
+
+and till now we have included no. of nonzero digits = nonzero
+
+where mask stores all the numbers that have taken in range [0....i-1],
+
+Using mask to make sure that all digits should be included ones
+
+Ans= n-(dp[0][1][0][0]-1)  
+
+// not consider 00000..0
+
+Note we are taking count of nonZero numbers becouse-:
+
+For 5 digit no:
+
+-----
+
+Possible no. = 00012 , 00013  
+
+[they are number 12,13 but while forming them we taken 0's multiple times, so we needs to take care that we can take multiple zeroes before any non zero digit
+
+]
+
+*/
+
+
+int dp[10][2][11][1025];
+
+class Solution {
+public:
+    int numDupDigitsAtMostN(int n) {
+    
+        string num=to_string(n);
+        
+         memset(dp,-1,sizeof(dp));
+        
+   int i=0,tight=1;
+    
+   return n-(find(i,tight,0,num,0)-1);
+   
+        // doing '-1' to ignore no =0
+        
+        
+        
+    }
+    
+    
+ int find(int i,int tight,int nonZero,string &num, int mask){
+
+  if(i==num.size()){
+   return 1;
+  }
+
+
+  int ub;  // upper bound digit
+
+  if(tight==1)
+  ub=num[i]-'0';
+  else
+  ub=9;
+
+       
+  if(dp[i][tight][nonZero][mask]!=-1)
+  return dp[i][tight][nonZero][mask];
+  
+  int ans=0;
+
+  for(int d=0;d<=ub;d++){
+ 
+       if(nonZero==0 || ((mask & (1<<d))==0)){
+           
+           if(d!=0)
+               nonZero++;
+           
+           mask=mask|(1<<d);
+      
+  ans=ans+find(i+1,tight && (d==ub),nonZero,num,mask);
+                   
+           if(d!=0)
+               nonZero--;
+           
+           mask=mask^(1<<d);
+      
+    
+       }
+
+ }
+                  
+ return dp[i][tight][nonZero][mask]=ans;
+
+ }
+
+    
+};
+
