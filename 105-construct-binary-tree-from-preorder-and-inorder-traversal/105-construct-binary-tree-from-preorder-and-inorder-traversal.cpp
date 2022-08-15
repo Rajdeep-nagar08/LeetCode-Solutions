d6aca5beea7d48568adaac6cdaf02032a9631cce
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-08-15 19:35:08 +0530

    Time: 16 ms (93.22%), Space: 26.3 MB (43.41%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,26 +1,11 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+/*
 
 class Solution {
 public:
      int i=0;
 
     TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
-        /*
-preorder:-
- root,left,right
-inorder:-
-left ,root,right
-*/
+    
       return build(pre,in,0,pre.size()-1);
     }
   
@@ -53,3 +38,51 @@ left ,root,right
       }
 };
 
+
+*/
+
+class Solution {
+public:
+    TreeNode*func(vector<int>& preorder,int preStart,int preEnd,vector<int>& inorder,int inStart, int inEnd, unordered_map<int,int>&in)
+    {
+        
+        if(preStart>preEnd || inStart>inEnd)
+        {
+            return NULL;
+            
+        }
+        
+        TreeNode*root= new TreeNode(preorder[preStart]);
+        
+        int inRoot= in[root->val];
+        
+        int left=inRoot-inStart;
+        
+        root->left=func(preorder,preStart+1,preStart+left,inorder,inStart,inRoot-1,in);
+        
+        root->right=func(preorder,preStart +left+1,preEnd,inorder,inRoot+1,inEnd,in);
+        
+        return root;
+        
+        
+    }
+    
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+    {
+           unordered_map<int,int>in;
+        
+        for(int i=0;i<inorder.size();i++)
+        {
+            
+            in[inorder[i]]=i;
+            
+            
+        }
+        
+    TreeNode*root= func(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,in);
+        
+        return root;
+        
+    }
+};
