83ffc9c6436f725c5ca06c338ea956d161fc0128
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-08-18 20:05:31 +0530

    Time: 7 ms (9.40%), Space: 6.3 MB (49.42%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.cpp
+++ 551-student-attendance-record-i/551-student-attendance-record-i.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    bool checkRecord(string s) {
-        
-        
-        int n=s.size();
-        
-         
-        int i=0;
-        
-        int a=0;
-        
-        while(i<n){
-            
-            if(s[i]=='P'){
-                i++;
-            }
-            else if(s[i]=='A'){
-                i++;
-                a++;
-                if(a>=2)
-                    return false;
-            }
-            else{
-                int l=0;
-                
-                while(i<n && s[i]=='L'){
-                    i++;
-                    l++;
-                    if(l>=3)
-                        return false;
-                }
+   bool checkRecord(string s) {
+       
+        int len=s.length();
+        int acount=0;
+        for(int i=0;i<len;i++){
+            if(s[i]=='A'){
+                acount++;
             }
         }
-        
-        return true;
+        if(acount>1){
+            return false;
+        }
+        //char ch='L';
+        int temp=0;
+        int count=0;
+        for(int i=0;i<len;i++){
+            if(s[i]=='L'){
+                count++;
+            }else{
+                temp=max(temp,count);
+                count=0;
+            }
+        }
+       
+       
+        if(temp>=3 || count>=3)
+        {
+            return false;
+            
+        }else{
+            return true;
+        }
     }
+    
 };
