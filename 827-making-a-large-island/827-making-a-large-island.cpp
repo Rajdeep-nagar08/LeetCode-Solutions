4048469e0eeac66224f00837279c047717de3a1c
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-08-21 13:33:11 +0530

    Time: 1906 ms (9.38%), Space: 332.2 MB (6.40%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.cpp
+++ 827-making-a-large-island/827-making-a-large-island.cpp
@@ -1,158 +1,136 @@
-/*
-
-let there is a component = [1,1,1,1,1]
-
-Using DFS , its size=5, and its our first component so we repace ecah '1'
-of this component by {5,1}
-
-Component = [{5,1},{5,1},{5,1},{5,1},{5,1}]
-
-We do same operations for each component
-
-e.g For another component [1,1,1] , size = 3 and its 2nd component we founds yet
-
-So we repalce its each value '1' by {3,2}
-
-[1,1,1] => [{3,2},{3,2},{3,2}]
-
-
--------------------------------------------
-
-
-NOW for each cell '0'
-
-We check it from all four directions whether there is any component that can be connected to it
-
-
-*/
-
-
-int dx[4]={1,-1,0,0};
-
-int dy[4]={0,0,1,-1};
-
 class Solution {
 public:
-    int n,m;
-    int largestIsland(vector<vector<int>>& grid) {
+    
+    /*
+         1 1 0 1     5 5 0 2
+         1 1 0 1  => 5 5 0 2
+         1 0 0 0     5 0 0 0
+         0 1 1 1     0 3 3 3
+    
+    */
+    
+    /*
+       [[0,0,0,0,0,0,0],
+        [0,1,1,1,1,0,0],
+        [0,1,0,0,1,0,0],
+        [1,0,1,0,1,0,0],
+        [0,1,0,0,1,0,0],
+        [0,1,0,0,1,0,0],
+        [0,1,1,1,1,0,0]]
+    */
+    
+    /*
+       [[1,0,1],
+        [0,0,0],
+        [0,1,1]]
+    */
+    
+    bool isValid(int i, int j, int n, int m) {
+        if(i >= 0 && j >= 0 && i < n && j < m) return true;
+        return false;
+    }
+    
+    vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
+    
+    int getCount(int i, int j, vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
         
+        if(isValid(i, j, n, m) == false || grid[i][j] == 0) return 0;
         
-        n=grid.size();
+        int count = 1;
+        grid[i][j] = 0;
         
-        
-        m=grid[0].size();
-        
-        int mx=INT_MIN;
-        
-        
-        vector<vector<pair<int,int>>>grid1(n,vector<pair<int,int>>(m));
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                grid1[i][j]={grid[i][j],0};
-            }
-        }
-        
-        
-        int p=0;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-             int count=0;
+        for(auto &it: dir) {
+            int ni = it.first + i;
+            int nj = it.second + j;
             
-             if(grid1[i][j].first==1){
-                dfs1(i,j,count,grid1);
-                p++;
-                dfs2(i,j,count,grid1,p);
-                 
-                 mx=max(mx,count);
-              }
-            }
+            count += getCount(ni, nj, grid);
         }
         
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid1[i][j].first==0){
-                    
-                    int a=0,p1=-1;
-                    int b=0,p2=-1;
-                    int c=0,p3=-1;
-                    int d=0,p4=-1;
-                    
-                    for(int i1=0;i1<4;i1++){
-                        int x1=i+dx[i1];
-                        int y1=j+dy[i1];
-                        
-            if(x1>=0 && x1<n && y1>=0 && y1<m && (grid1[x1][y1].first!=0)){
-                 
-                int cnt=grid1[x1][y1].first;
-                int val=grid1[x1][y1].second;
-                
-                
-                        if(a==0){
-                            a=cnt;
-                            p1=val;
-                        }
-                       
-                else if(p1!=val && (b==0)){
-                         b=cnt;
-                         p2=val;
-                     }
-                
-          else if((p1!=val) && (p2!=val) && (c==0)){
-              
-                c=cnt;
-                p3=val;
-                    
-                }
-                
-         else if((p1!=val) && (p2!=val) && (p3!=val) && (d==0)){
-             
-             d=cnt;
-             p4=val;
-         }
-                  
-                  }
-                    
-                }
-                    
-              mx=max(mx,a+b+c+d+1);
-                    
-                }
-               
-                
-            }
-        }
-        
-        return mx;
-        
-        
+        return count;
     }
     
-    
-    void dfs1(int i,int j,int &count,vector<vector<pair<int,int>>>&grid1){
-        count++;
-        grid1[i][j].first=-1;
-        for(int x=0;x<4;x++){
-            int x1=i+dx[x];
-            int y1=j+dy[x];
-            if(x1>=0 && x1<n && y1>=0 && y1<m && (grid1[x1][y1].first==1)){
-                dfs1(x1,y1,count,grid1);
-            }
+    void fillCount(int i, int j, int count, vector<vector<int>>& area, 
+                   vector<vector<int>>& grid, vector<vector<int>> &mark, int marker) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        if(isValid(i, j, n, m) == false || grid[i][j] == 0) return;
+        
+        grid[i][j] = 0;
+        area[i][j] = count;
+        mark[i][j] = marker;
+        
+        for(auto &it: dir) {
+            int ni = it.first + i;
+            int nj = it.second + j;
+            
+            fillCount(ni, nj, count, area, grid, mark, marker);
         }
     }
     
+    int getVal(int i, int j, vector<vector<int>>& area, vector<vector<int>>& mark) {
+        int n = area.size();
+        int m = area[0].size();
+        int val = 1;
+        
+        unordered_set<int> st;
+        
+        for(auto &it: dir) {
+            int ni = i + it.first;
+            int nj = j + it.second;
+            
+            if(isValid(ni, nj, n, m) == false) continue;
+            int marker = mark[ni][nj];
+
+            if(st.find(marker) != st.end()) continue;
+
+            st.insert(marker);
+            val += area[ni][nj];
+        }
+        return val;
+    }
     
-void dfs2(int i,int j,int &count,vector<vector<pair<int,int>>>&grid1,int &p){
-        grid1[i][j].first=count;
-        grid1[i][j].second=p;
-        for(int x=0;x<4;x++){
-            int x1=i+dx[x];
-            int y1=j+dy[x];
-            if(x1>=0 && x1<n && y1>=0 && y1<m && (grid1[x1][y1].first==-1)){
-                dfs2(x1,y1,count,grid1,p);
+    int largestIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> copy = grid;
+        vector<vector<int>> area(n, vector<int>(m, 0));
+        vector<vector<int>> mark(n, vector<int>(m, 0));
+        
+        int marker = 1;
+        int mxCount = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 0) continue;
+                
+                int count = getCount(i, j, grid);
+                
+                mxCount = max(mxCount, count);
+                
+                fillCount(i, j, count, area, copy, mark, marker);
+                marker++;
             }
         }
+        
+        // for(auto&v: area) {
+        //     for(auto &it: v) cout << it << " ";
+        //     cout << endl;
+        // }
+        
+        int ans = mxCount;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(area[i][j] != 0) continue;
+                
+                int val = getVal(i, j, area, mark);
+                if(val == 5) cout << i << " " << j << endl;
+                ans = max(ans, val);
+            }
+        }
+        
+        return ans;
     }
 };
