2610352a7030c7cfdc2fc5beee5e5ba10682bac9
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-08-30 02:01:05 +0530

    Time: 548 ms (6.12%), Space: 17.6 MB (16.28%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numTilePossibilities(string str) {
+        
+        vector<string>v;
+        
+        sort(str.begin(),str.end());
+        
+        
+        do{
+            v.push_back(str);
+        }while(next_permutation(str.begin(),str.end()));
+        
+        
+        int n=str.size();
+        
+        unordered_set<string>st;
+        
+        for(int mask=1;mask<=(1<<n)-1;mask++){
+            
+            for(string str:v){
+                
+                string temp;
+                
+                for(int i=0;i<n;i++){
+                    
+                    if(mask & (1<<i)){
+                        temp+=str[i];
+                    }
+                }
+                
+                st.insert(temp);
+            }
+        }
+        
+        return (int)st.size();
+        
+    }
+};
