2b0dd8ba493df80bc9e9f8d9c9f7a56393a1c737
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-09-16 18:32:53 +0530

    Time: 7 ms (93.20%), Space: 10.1 MB (24.49%) - LeetHub

---
+++ 893-groups-of-special-equivalent-strings/893-groups-of-special-equivalent-strings.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    int numSpecialEquivGroups(vector<string>& words) {
+       
+                
+        /*
+        
+        swap (even <=> even) OR (odd <=> odd) indexes
+        
+        
+        no. of sets of string such that each string is pairwise equivalent to other
+        
+        
+        
+        basically we needs to focus on chars at even and odd positions only
+        
+        
+        str1   ===  str2
+        
+        if (chars at even position of str1 == chars at even position of str2)
+        
+        if(chars at odd position of str1 == chars at odd position of str2)
+        
+        order of chars do not matter
+        
+        */
+        
+        
+      unordered_map<string ,unordered_map<string,int>>mp;  // chars at even pos => (char at odd pos , count of such strings)
+        
+        
+        for(string str:words){
+            
+            string even,odd;
+            
+            for(int i=0;i<str.size();i++){
+                
+                if(i%2==0)
+                    even+=str[i];
+                else
+                    odd+=str[i];
+            }
+            
+            sort(even.begin(),even.end());
+            
+            sort(odd.begin(),odd.end());
+            
+       //     cout<<even<<" "<<odd<<endl;
+            
+            mp[even][odd]++;
+            
+        }
+        
+        
+        int ans=0;
+        
+        for(auto it:mp){
+          /*  
+            cout<<it.first<<"->";
+            
+            for(auto it1:it.second){
+                
+                cout<<it1.first<<" "<<it1.second<<endl;
+                
+            }
+            */
+          ans+=it.second.size();
+            
+        }
+        
+        return ans;
+        
+    }
+};
