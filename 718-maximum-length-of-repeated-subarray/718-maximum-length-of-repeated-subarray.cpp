f5d692cee81b41668385988539514f58fee3bdb7
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-09-20 23:39:14 +0530

    Time: 764 ms (17.31%), Space: 15.7 MB (90.04%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,39 +1,75 @@
+/*
+
+for each index i in nums1, we have some choices over nums2 to start
+
+dp[i][j]= common length we can get from [i...n1-1] and [j....n2-1]
+
+ans=max(dp[i][j])
+
+*/
+
+int dp[1001][1001];
+
+int n1,n2;
+
+int ans;
 
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
-        int n2=nums2.size();
         
-        vector<vector<int>>dp(n1+1, vector<int>(n2+1));
-        int ans=0;
         
+        ans=0;
+        
+        n1=nums1.size();
+        
+        n2=nums2.size();
+        
+        memset(dp,-1,sizeof(dp));
+                
+        find(0,0,nums1,nums2);
+    
         for(int i=0;i<=n1;i++){
             for(int j=0;j<=n2;j++){
-        if(i==0 || j==0)
-            dp[i][j]=0;
-                
-        else if(nums1[i-1]==nums2[j-1]){
-          dp[i][j]=dp[i-1][j-1]+1;
-            ans=max(ans,dp[i][j]);
+                ans=max(ans,dp[i][j]);
+            }
         }
-            else
-           dp[i][j]=0;//max(dp[i][j],dp[i-1][j-1]);
-                
-            
-                         }
-                        }
-                       
-            return ans;
         
+        return ans;
+        
+    }
+    
+    int find(int i,int j,vector<int>&nums1,vector<int>&nums2){
+        
+        if(i>=n1 || j>=n2)
+            return 0;
+        
+       
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+              
+        if(nums1[i]==nums2[j]){
+            
+         int ch1=1+find(i+1,j+1,nums1,nums2);
+            
+         find(i+1,j,nums1,nums2);
+        
+         find(i,j+1,nums1,nums2);
+                        
+            return dp[i][j]=ch1;
+            
+        }
+        
+        else{
+            
+        find(i+1,j,nums1,nums2);
+        
+        find(i,j+1,nums1,nums2);
+
+        return dp[i][j]=0;
+            
+        }
         
     }
 };
-
-/*
-
-dp[i][j]= maximum length repeated subarray in array of length i and length j
-
-dp[i][j]=max(dp[i][j],dp[i-1][j-1]+(str[i]==str[j]?1:0)
-
-*/
