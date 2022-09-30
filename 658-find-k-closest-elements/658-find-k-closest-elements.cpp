e8f9661969604ac58788ad577e4412ff84103fc2
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-09-30 18:25:52 +0530

    Time: 213 ms (10.69%), Space: 34.1 MB (37.25%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-     // TC= O(nlogn) , SP= O(n)
-      /*
-      vector<pair<int,int>>v;
-      for(int y:arr)
-        v.push_back({abs(x-y),y});
-      
-      sort(v.begin(),v.end());
-
-        vector<int>ans(k);
-      for(int i=0;i<k;i++)
-        ans[i]=v[i].second;
-      
-      sort(ans.begin(),ans.end());
-      
-      return ans;
-      */
-      
-      // TC= O(nlogk) , SC=O(k)
-      
-      priority_queue<pair<int,int>>pq;
-      
-      for(int i=0;i<k;i++)
-        pq.push({abs(x-arr[i]),arr[i]});
-      
-      for(int i=k;i<arr.size();i++){
-        pq.push({abs(x-arr[i]),arr[i]});
-        pq.pop();
-      }
-      
-      vector<int>ans;
-      
-      while(pq.size()>0){
-      ans.push_back(pq.top().second);
-        pq.pop();
-      }
-      sort(ans.begin(),ans.end());
-      return ans;
+     
+     int n=arr.size();
+        
+    priority_queue<pair<int,int>>pq;
+        
+        for(int i=0;i<n;i++){
+            
+            pq.push({abs(arr[i]-x),i});
+            
+            if(pq.size()>k)
+                pq.pop();
+            
+        }
+        
+        vector<int>ans;
+        
+        while(pq.size()>0){
+            int idx=pq.top().second;
+            
+            ans.push_back(arr[idx]);
+            
+            pq.pop();
+            
+        }
+        
+        sort(ans.begin(),ans.end());
+        
+        return ans;
+        
+        
     }
 };
-
