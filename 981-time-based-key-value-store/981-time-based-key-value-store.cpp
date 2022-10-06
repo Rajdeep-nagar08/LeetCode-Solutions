264ebc735ed711afd0ffc44dd006768d31519f1a
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-10-06 10:25:38 +0530

    Time: 774 ms (18.01%), Space: 141 MB (5.03%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,78 +1,35 @@
-/*
-All the timestamps timestamp of set are strictly increasing.
-So to get a value of perticular key and timestamp , we can do binary search for timestamp
-
-Note: upper bound can be apply either on map or vector ( not on unordered map)
-*/
-/*
 class TimeMap {
-  unordered_map<string,map<int,string>>mp;  // key->{timestamp,value}
-  // instead of inside map, we can also use vector of pair ( becouse timestamp wiil already stored in sorted order as per the qusetion)
-  
 public:
+    
+    unordered_map<string,vector<pair<int,string>>>mp;
+    
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        mp[key].insert({timestamp,value});
+        
+       mp[key].push_back({timestamp,value}); // foo => {1,bar} , {4,bar2}
+        
     }
-  
-  Imagine we have the following timestamps: [5, 10, 15].
-
-If we query, say, 20, upper_bound returns end(), so the result is the label for 15 (prev(end())).
-Now, if we query 3, upper_bound returns begin(), and that means we do not have a label (the result is an empty string).
-
-let we were given 7, upper_bound will give 10 and prev(10)=5 , which is the ans for 7
-
-
-Another interesting case is when we query the exact timestamp, say, 10. upper_bound will return 15 and prev(15)
-
-Note: we can also use lower bound : in that case we need to take care of equality also
-
     
-  
     string get(string key, int timestamp) {
         
-      auto it= mp[key].upper_bound(timestamp);
-      
-      if(it==mp[key].begin())
-        return "";
-      
-      it--;
-      
-      return it->second;
-    }
-};
-*/
-
-class TimeMap {
-   unordered_map<string,vector<pair<int,string>>>mp;
-public:
-    TimeMap() {
+       // vector<pair<int,string>>v=mp[key];
+                
+        if(mp[key].size()==0)
+            return "";
+        
+pair<int,string>p= {timestamp,"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"};
+        
+        int i=upper_bound(mp[key].begin(),mp[key].end(),p)-mp[key].begin();
+         
+       i--;
+        
+        if(i<0)
+            return "";
+        
+        return mp[key][i].second;
         
     }
-    
-    void set(string key, string value, int timestamp) {
-      mp[key].push_back({timestamp,value});
-    }
-
-string get(string key,int timestamp) {
-  
-  auto it= upper_bound(mp[key].begin(),mp[key].end(),pair<int,string>{timestamp,""},
-                      [](const pair<int,string>&p1 , const pair<int,string>&p2 ){
-                         return p1.first<p2.first;
-                      });
- 
-  
-  if(it==mp[key].begin())
-    return "";
-  
-  it--;
-  
-  return it->second;
-}
-  
 };
-
-
