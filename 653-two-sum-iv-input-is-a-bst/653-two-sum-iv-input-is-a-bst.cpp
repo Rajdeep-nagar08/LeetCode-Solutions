6d70b4a6850c646ed192664fd3c68bb4a19a12e6
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-10-09 07:37:31 +0530

    Time: 87 ms (36.59%), Space: 36 MB (98.68%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,42 +9,72 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// O(n) time , O(n) space
+/*
 class Solution {
+  vector<int>in;
 public:
-    vector<int>v;
-    
     bool findTarget(TreeNode* root, int k) {
-        
-       find(root);
-        
-    
-        int i=0,j=v.size()-1;
-        
-        
-        while(i<j){
-           if(v[i]+v[j]==k)
-               return true;
-            
-            else if(v[i]+v[j]>k)
-                j--;
-            
-            else
-                
-                i++;
-        }
-        
-        return false;
+        inorder(root);
+      return search(k);
     }
     
-    void find(TreeNode* root){
-        if(!root)
-            return;
-        
-        find(root->left);
-        
-        v.push_back(root->val);
-        
-        find(root->right);
-        
+  void inorder(TreeNode* root){
+    if(!root)
+      return;
+    inorder(root->left);
+    in.push_back(root->val);
+    inorder(root->right);
+  }
+   
+  bool search(int k){
+    int n=in.size();
+    
+    int i=0,j=n-1;
+    while(i<j){
+      int sum=in[i]+in[j];
+      if(sum==k)
+        return true;
+      else if(sum>k)
+        j--;
+      else
+        i++;
+    }
+    return false;
+  }
+};
+*/
+  
+/*
+O(n*h) time
+o(h) space
+*/
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+  
+      return find(root,root,k);
+    }
+     
+  bool  find(TreeNode* root,TreeNode* curr,int k){
+      if(!curr)
+        return false;
+      
+      return find(root,curr->left,k)||find(root,curr->right,k)||
+        search(root,curr,k-curr->val);
+    }
+  
+    bool search(TreeNode* root,TreeNode* curr,int key){
+      if(!root)
+        return false;
+      
+      if(root->val==key && root!=curr)
+        return true;
+      
+      if(root->val > key)
+        return search(root->left,curr,key);
+      
+      return search(root->right,curr,key);
     }
 };
