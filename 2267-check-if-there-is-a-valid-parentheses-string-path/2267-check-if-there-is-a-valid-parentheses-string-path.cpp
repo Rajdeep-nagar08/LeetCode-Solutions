2d32a82a8f932b257b4e1b75938469cb504ca29d
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-10-14 09:11:42 +0530

    Time: 473 ms (46.25%), Space: 33.4 MB (46.96%) - LeetHub

---
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -0,0 +1,138 @@
+/*
+
+Using Backtracking 
+
+ 
+         when reached at [n-1,m-1] check the string using stack whether it is balanced         or not
+         
+         Finally return "Yes" if stack size ==0
+         
+         otherwise return back and try another pass
+         
+Give TLEEEEEEEEEE
+
+
+from [i,j] we have two choices to move => right or down
+
+if [0,0] to [i,j] is balanced than ([i+1,j] or [i,j+1]) to [n-1,m-1] should be balanced
+
+if [0,0] to [i,j] is not balanced than [0,0] to [n-1,m-1] should be balanced
+
+dp[i][j][prev]= is path from ([i+1,j] or [i,j+1]) to [n-1,m-1] balanced or not, prev= path 
+
+from [0,0] to [i,j] is balanced or not
+
+*/
+
+int dp[101][101][501];
+
+class Solution {
+public:
+    
+    int n,m;
+    
+    int dx[2]={1,0};
+    
+    int dy[2]={0,1};
+    
+    bool hasValidPath(vector<vector<char>>& grid) {
+    
+         n=grid.size();
+        
+         m=grid[0].size();
+        
+        if(grid[0][0]==')')
+            return false;
+        
+        memset(dp,-1,sizeof(dp));
+    
+        
+        if(find(0,0,1,grid))
+            return true;
+        
+        return false;
+        
+    }
+    
+      int find(int i,int j,int open,vector<vector<char>>&grid){
+         
+          if(i==n-1 && j==m-1){
+              
+              //cout<<str<<endl;
+              
+              if(open==0)
+                  return 1;
+              
+              return 0;
+        
+              
+          }
+         
+          
+          if(dp[i][j][open]!=-1)
+              return dp[i][j][open];
+         
+          
+          int ans=0;
+          
+          int open1=open;
+          
+          for(int i1=0;i1<2;i1++){
+              
+              int x1=i+dx[i1];
+              
+              int y1=j+dy[i1];
+             
+              if(x1>=n || y1>=m || x1<0 || y1<0)
+                   continue;
+                 
+                 if(grid[x1][y1]==')')
+                 {
+                     if(open-1>=0)
+                         ans=ans|find(x1,y1,open-1,grid);
+                 }
+              
+              else{
+                  
+                  
+                ans=ans|find(x1,y1,open+1,grid);
+
+                  
+              }
+              
+        }
+              
+          
+          return dp[i][j][open1]=ans;
+         
+      }
+    
+    
+    bool isbalanced(string str){
+        
+        stack<char>st;
+                      
+              for(char ch:str){
+                  if(ch=='(')
+                        st.push('(');
+                     
+                  else{
+                      if(st.size()==0)
+                           return false;
+    
+                      if(st.top()==')')
+                          st.push(')');
+                      else
+                          st.pop();
+                  }
+              }
+              
+        
+              if(st.size()>0)
+                  return false;
+        
+        
+        return true;
+              
+    }
+};
