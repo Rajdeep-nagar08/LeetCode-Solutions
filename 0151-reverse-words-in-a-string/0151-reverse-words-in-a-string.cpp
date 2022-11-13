4a5d326d1b12fac1dad1373bb4271c87b3771dea
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-11-13 11:24:48 +0530

    Time: 14 ms (48.01%), Space: 7.5 MB (71.22%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,63 +1,20 @@
-/*
-reverse --> [begin,end)
-s.begin()+0 or s.begin() means 0th index so s.begin()+i means ith index
-s.end() means nth index , where n= length of string ( points outside of string)
-
-erase---> [begin,end) erase all characters of this range
-
-*/
-
 class Solution {
 public:
     string reverseWords(string s) {
     
-      int i=0;
-      
-      //removing front spaces
-      while(s[i]==' ' && i<s.length())
-        i++;
-      
-      if(i!=0)
-      s.erase(s.begin(),s.begin()+i);
-      
-    //  removing last spaces
-      i=0;
-      while(s[s.length()-1-i]==' ' && i<s.length())
-        i++;
-      
-      if(i!=0)
-      s.erase(s.end()-i,s.end());
-       
-    //reverse whole string
-      reverse(s.begin(),s.end());
-      
-     i=0;
-      while(i<s.length()){
         
-        //reverse each words
-        int start=i;
-        while(s[i]!=' ' && i<s.length())
-          i++;
+        reverse(s.begin(),s.end());
         
-    
-        if(start!=i)
-          reverse(s.begin()+start,s.begin()+i);
         
-        // remove extra spaces between two words
-        start=i;
-        while(s[i]==' ' && i<s.length())
-          i++;
+stringstream ss(s); 
+string temp;
+string str="";
+while(ss>>temp){
+    str+=" ";
+    reverse(temp.begin(),temp.end());
+str+=temp;
+}
         
-        if(i-start<=1)
-          continue;
-        else
-          s.erase(s.begin()+start+1,s.begin()+i);
-        
-        i=start+1;
-          
-          
-      }
- 
-      return s;
+        return str.substr(1);
     }
 };
