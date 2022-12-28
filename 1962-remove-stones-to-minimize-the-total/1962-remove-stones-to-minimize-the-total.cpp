57396bff4a36e93428eb2110ffabcaa57617b536
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-12-28 12:45:08 +0530

    Time: 552 ms (91.38%), Space: 105 MB (55.34%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
-      
-        /*
-        as we needs to remove max to max stones 
-        so we always pick max piles and remove x/2 stones from it
-        
-        */
-        
-    priority_queue<int>p;
 
-        
-        int sum=0;
-        for(int x:piles){
-            sum+=x;
-            p.push(x);
-            
-        }
-        
-        while(k>0 && p.size()>0){
-            
-            int t=p.top();
-            p.pop();
-            
-            int rmv=t/2;
-            sum=sum-rmv;
-            int left=t-rmv;
-            if(left>0)
-                p.push(left);
-            
-            k--;
-        }
-        
-        return sum;
-        
+            /*
+            as we needs to remove max to max stones 
+            so we always pick max piles and remove x/2 stones from it
+
+            */
+
+        priority_queue<int>p;
+
+
+            int sum=0;
+            for(int x:piles){
+                sum+=x;
+                p.push(x);
+
+            }
+
+            while(k>0 && p.size()>0){
+                int t=p.top();
+                p.pop();
+                int rmv=t/2;
+                sum=sum-rmv;
+                int left=t-rmv;
+                if(left>0)
+                    p.push(left);
+
+                k--;
+            }
+
+            return sum;
+
     }
 };
