58c43f93a58a53bde7716d3f6d3755317fc85f40
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2022-12-30 09:36:33 +0530

    Time: 0 ms (100.00%), Space: 6 MB (51.39%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -3,45 +3,58 @@ public:
     string getPermutation(int n, int k) {
         
         
-        vector<int>v(n);
+        vector<int>fact(n+1);
+                
+        fact[1]=1;
         
-        for(int i=0;i<n;i++){
-          v[i]=i+1;
+        for(int i=2;i<=n;i++){
+            fact[i]=fact[i-1]*i;
         }
+    
+         string str;
         
-        while(k>1){
-            nextPer(v);
-            k--;
+        for(int i=1;i<=n;i++){
+            str.push_back(i+'0');
         }
+               
         string ans;
         
-        for(int x:v){
-            ans.push_back(x+'0');
+        while(true){
+       
+             n=str.size();
+            
+            if(n==1)
+                break;
+            
+            if(k>fact[n])
+            {
+                if(k%fact[n]==0)
+                    k=fact[n];
+                else
+                k=k%fact[n];
+            }
+
+            int i=k/fact[n-1]+(k%fact[n-1]==0?0:1);
+            
+            i--;
+                           
+            ans.push_back(str[i]);
+    
+            string s;
+            
+            for(int i1=0;i1<n;i1++){
+                if(i1!=i)
+                    s.push_back(str[i1]);
+            }
+            
+            str=s;
+                        
         }
+        
+        ans.push_back(str[0]);
+        
+        
         return ans;
-    }
-    
-    
-  void nextPer(vector<int>& nums) {
-    
-       int n=nums.size();
-      int i;
-      for(i=n-2;i>=0;i--){
-        if(nums[i]<nums[i+1])
-          break;
-      }
-       
-      if(i==-1)  // whole array in decreasing order, so return reverse of it
-        return reverse(nums.begin(),nums.end());
-      
-      // reverse the array part which is in decreasing order
-         reverse(nums.begin()+i+1,nums.end());
-      
-        // find next greater to pivot and reverse it with that 
-      auto it=upper_bound(nums.begin()+i+1,nums.end(),nums[i]);
-      
-      swap(*it,nums[i]);
-       
     }
     
 };
