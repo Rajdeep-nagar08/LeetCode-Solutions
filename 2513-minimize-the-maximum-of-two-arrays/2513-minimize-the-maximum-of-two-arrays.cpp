fe38d061d92ca30aa0daa1892d9007ae4fdc5c17
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-01-06 01:53:12 +0530

    Time: 0 ms (100.00%), Space: 6 MB (7.60%) - LeetHub

--- 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.cpp
+++ 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int minimizeSet(int div1, int div2, int cnt1, int cnt2) {
-       
-        
-     //    cout<<find(41,59,1,7609)<<endl;
-    /// cout<<find(6,9,1,50)<<endl;
-        
+  
         int l=0,h=INT_MAX;
         
         int ans;
@@ -21,16 +17,12 @@ public:
 //             if(mid>h)
 //                 break;
             
-         //  cout<<l<<" "<<h<<" "<<mid;
 
             if(isValid(mid,div1,div2,cnt1,cnt2)){
-             //   cout<<" Yes"<<endl;
                ans=mid;
                 h=mid-1;
             }
             else{
-              //  cout<<" NO"<<endl;
-
                l=mid+1; 
             }
         }
@@ -40,25 +32,7 @@ public:
     }
     
     bool isValid(int mid,int d1,int d2,int c1,int c2){
-        
-        
-//           if(mid%d1==0 && mid%d2==0)
-//              {
-//                  cout<<mid<<endl;
-              
-//                  return false;
-//              }
-        
-//         if(mid%d1==0){
-//             mid--;
-//             c2--;
-//         }
-        
-//         else if(mid%d2==0){
-//             mid--;
-//             c1--;
-//         }
-        
+       
         int f1=mid/d1;
     
         if(mid-f1<c1)
@@ -71,24 +45,11 @@ public:
         
   
         int cm=find(d1,d2,1,mid);
-              
-    
-//         if(mid==7609)
-//             cout<<cm<<endl;
-        
+            
         int totalAvl=mid-cm;
         
         if(totalAvl<c1+c2)
             return false;
-        
-//         if(rem<c1+c2)
-//             return false;
-    
-//         if(rem-f1<c1)
-//             return false;
-        
-//         if(rem-f2<c2)
-//             return false;
         
         return true;
         
