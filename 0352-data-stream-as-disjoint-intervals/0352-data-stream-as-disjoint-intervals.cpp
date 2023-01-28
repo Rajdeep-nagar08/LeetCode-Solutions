c4c38343e0810cb14e8a6ff392595a9a47196ed1
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-01-28 12:23:59 +0530

    Time: 110 ms (33.33%), Space: 31.9 MB (92.16%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -4,6 +4,56 @@ public:
 
     unordered_map<int,int>mp1,mp2,mp3;
     
+    /*
+
+    l=mp1[value]= starting number of any range that is ending at 'value'
+    or any range like [l...value]
+    
+    r=mp2[value]= last number of any range that is starting from 'value'
+    or any range like [value....r]
+    
+    So there can be 4 cases:
+    
+    case 1:
+    
+      
+      'value' cannot be part of any range
+      so push [value,value] in the range
+      
+     case 2:
+       there is a range of type [l....value-1] but there is no range that is starting from value+1
+       
+       so add 'value' at the last of [l...value-1]
+       
+       and replace the range [l...value-1] by [l,value]
+       
+      case3:
+      
+        there is no range like [l....value-1], but there is a range like [value+1.....r]
+        so add 'value' at the starting of range [value+1....r]
+        
+        and replace the range [value+1,r] by [value,r]
+    
+       case 4:
+       
+        there are two ranges like [l....value-1]  and [value+1....r]
+        
+        so add 'value' at the middle of both
+        
+        and replace both the ranges by [l,r]
+        
+        
+    
+    
+    mp3[value] , whether value is already present in range or not
+    
+    if value is already a part of any range, then avoid it
+    
+    
+    */
+    
+    
+    
     vector<vector<int>>ans;
     
     SummaryRanges() {
@@ -28,10 +78,7 @@ public:
         if(mp2.count(value+1)==1){
             r=mp2[value+1];
         }
-          
-        
-       // cout<<value<<" "<<l<<" "<<r<<endl;
-        
+                  
         if(l==-1 && r==-1){
             
             mp1[value]=value;
@@ -58,9 +105,7 @@ public:
             mp2[value]=value;
             mp1[value]=l;
             mp2[l]=value;
-            
-            // [l,value-1] => [l,value]
-            
+                        
             vector<int>v={l,value-1};
             
             auto i1=lower_bound(ans.begin(),ans.end(),v)-ans.begin();
@@ -76,10 +121,6 @@ public:
             
             mp1[value]=value;
             
-           // cout<<value<<" "<<mp2[value]<<endl;
-            
-            // [value+1,r] => [value,r] 
-
              vector<int>v={value+1,r};
             
             auto i1=lower_bound(ans.begin(),ans.end(),v)-ans.begin();
@@ -89,10 +130,7 @@ public:
         }
         
         else{
-            
-            
-            // [l,value-1] , [value+1,r] => [l,r]
-            
+                        
             mp2[l]=r;
             
             mp1[r]=l;
@@ -102,9 +140,7 @@ public:
              auto i1=lower_bound(ans.begin(),ans.end(),v)-ans.begin();
             
             ans[i1]={l,r};
-            
-          //  cout<<value<<" "<<l<<" "<<r<<endl;
-            
+                        
             for(int j=i1+1;j<ans.size()-1;j++){
                 ans[j]=ans[j+1];
             }
