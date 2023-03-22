06dcd6358c2d8ea8bd1b443a5b3eede5b769b387
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-03-22 11:52:43 +0530

    Time: 166 ms (12.59%), Space: 372.6 MB (6.67%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.cpp
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.cpp
@@ -16,25 +16,30 @@ public:
         int i=0;
         
         lln make=0;
+        
         string ans;
+
         while(i<k){
 make=(make%mod + ((s[i]-'a'+1)*power[k-1-i])%mod)%mod;
             i++;
         }
         
-        if((make%mod) ==(hash%mod))
+        if((make%mod) ==(hash))
             ans=s.substr(0,k);
     
         int j=0;
         
         while(i<s.length()){
+            
  make=(make%mod-((s[j]-'a'+1)*power[k-1])%mod+mod)%mod;
+            
             make=(make*p)%mod;
+            
             make=(make%mod + (s[i]-'a'+1)%mod)%mod;
             
             j++;
         
-            if((make%mod)==(hash%mod))
+            if((make%mod)==(hash))
                 ans= s.substr(j,k);
             
             i++;
