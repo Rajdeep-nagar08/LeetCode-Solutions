61670faa1edebff9aa514b7bd2de3d7e8e71e0eb
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-04-05 22:47:34 +0530

    Time: 35 ms (13.66%), Space: 144 MB (7.32%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
@@ -2,37 +2,23 @@ class Solution {
 public:
     bool queryString(string s, int n) {
         
-          
         vector<bool>v(n+1,false);
-        
         
         int sz=log2(n)+1;
         
-        int m=s.size();
-        
-         // cout<<sz<<endl;
-        
+        int m=s.size();        
         
         for(int l=1;l<=sz;l++){
             
             for(int i=0;i<m;i++){
                 
                 if(i+l-1<m){
-                    
-                    // cout<<i<<" "<<l<<endl;
-                    
+                                        
                 string str=s.substr(i,l);
-                    
-
-                   // reverse(str.begin(),str.end());
-                    
-                  // cout<<str<<endl;
-
-                    
+                
                   int num = stoi(str, 0, 2);
                     
                     if(num<=n && (v[num]==false)){
-                        // cout<<num<<endl;
                         v[num]=true;
                     }
 
