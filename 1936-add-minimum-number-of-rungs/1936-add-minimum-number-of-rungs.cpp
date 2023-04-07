ca3f347d732e57b19bd337836c3d837978ac292d
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-04-07 23:43:24 +0530

    Time: 121 ms (25.65%), Space: 54.2 MB (32.61%) - LeetHub

--- 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.cpp
+++ 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.cpp
@@ -5,21 +5,7 @@ public:
         
         int n=rungs.size();
         
-                int ans=0;
-
-        
-//         if(dist==1){
-            
-            
-//         if(rungs[0]!=0)
-//         ans+=rungs[0]-1;
-            
-//             for(int i=1;i<n;i++){
-//                 ans+=rungs[i]-rungs[i-1]-1;
-//             }
-            
-//             return ans;
-//         }
+        int ans=0;
         
         if(rungs[0]-0>dist)
         ans+=(rungs[0]-0)/dist-(rungs[0]%dist?0:1);
