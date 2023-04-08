2c374a57fe42ba06a2ebab1eb5260cc0d1f5dab0
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-04-08 23:56:42 +0530

    Time: 221 ms (84.22%), Space: 48.6 MB (40.87%) - LeetHub

---
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int maximumTastiness(vector<int>& v, int k) {
+        
+        int n=v.size();
+        
+        
+        if(v.size()==1 || v.size()<k)
+            return 0;
+        
+        sort(v.begin(),v.end());
+        
+        int l=0,h=1e9;
+        
+        int ans;
+        
+        while(l<=h){
+            
+            int mid=l+(h-l)/2;
+            
+            if(isValid(mid,v,k)){
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return ans;
+    }
+    
+    
+    bool isValid(int mid,vector<int>&v,int k){
+        
+        
+        int n=v.size();
+        
+        int x=v[0];
+        
+        int cnt=1;
+        
+        for(int i=1;i<n;i++){
+            if(v[i]-x>=mid){
+                x=v[i];
+                cnt++;
+            }
+        }
+        
+        
+        if(cnt>=k)
+            return true;
+        
+        return false;
+        
+    }
+};
