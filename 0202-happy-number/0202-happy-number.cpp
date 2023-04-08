742af1d1063b8d35246b8f8e7f2ec987c44f9c3e
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-04-08 22:47:32 +0530

    Time: 321 ms (6.56%), Space: 6 MB (77.12%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+        
+        
+        string str=to_string(n);
+        
+        
+        int t=0;
+        
+        while(t<=5000){
+            int sum=0;
+            for(char ch:str){
+                sum+=(ch-'0')*(ch-'0');
+            }
+            
+            if(sum==1)
+                return true;
+            
+            if(sum==0)
+                return false;
+            
+            str=to_string(sum);
+            
+            t++;
+        }
+        
+        return false;
+        
+    }
+};
