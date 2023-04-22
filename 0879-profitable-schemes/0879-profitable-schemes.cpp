61a0ead5acd81ac50ee094e15bba46c30d1bde53
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-04-22 16:33:58 +0530

    Time: 186 ms (55.14%), Space: 16.4 MB (41.59%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,25 +1,63 @@
+int mod=1e9+7;
+
+#define lln long long int
+
+lln dp[102][102][102];  
+
 class Solution {
 public:
-   int dp[102][102][102];
-    int mod=1e9+7;
-    int solve(int i,int currPeople, int currProfit,int totalP, int minProfit, vector<int>& group, vector<int>& profit)
-    {
-        if(i==profit.size())
-        {
-            if(currProfit>=minProfit and totalP>=currPeople) return 1;
-            return 0;
-        }
-        else if(totalP<currPeople) return 0;
-     
-        if(dp[i][currPeople][currProfit]!=-1) return  dp[i][currPeople][currProfit];
-        int include=0,notInclude=0;
-        notInclude= solve(i+1,currPeople,currProfit,totalP,minProfit,group,profit);
-        include=solve(i+1,currPeople+group[i],min(currProfit+profit[i],minProfit),totalP,minProfit,group,profit);
-        return  dp[i][currPeople][currProfit] = (include  % mod + notInclude  % mod )%mod;
-    }
-    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,0,n,minProfit,group,profit);
+    int profitableSchemes(int n, int minP, vector<int>& group, vector<int>& prof) {
+        
+        /*
+        
+        sum of groups <= n
+        
+        sum of profits of each group >= minProfit
+        
+        
+        Count of such types of groups =?
+        
+        For each groups we have two choices => either include it in current groups or not include it in current group
+        
+        
+        dp[i][size][prof]= no of ways to choose from arr[i.....n-1] if
+          
+          if size of prev group= size <= n(100 max)
+          
+          and profit of prev group = prof >= minP (100 max)
+ 
+    
+    */
+        
+      memset(dp,-1,sizeof(dp));
+        
+        return find(0,0,0,n,minP,group,prof)%mod;
         
     }
+    
+      int find(int i,int sz,int prevP, int n, int minP, vector<int>&group, vector<int>&prof){
+          
+          if(i>=(int)group.size()){
+              if(prevP>=minP)
+              return 1;
+              
+              return 0;
+          }
+          
+          if(dp[i][sz][prevP]!=-1)
+              return dp[i][sz][prevP];
+          
+          lln count=0;
+ 
+          
+          count+= find(i+1,sz,prevP,n,minP,group,prof);
+          
+          
+          if(group[i]+sz<=n)
+      count+= find(i+1,group[i]+sz,min(prevP+prof[i],101),n,minP,group,prof);
+          
+          
+          return dp[i][sz][prevP]=count%mod;
+          
+      }
 };
