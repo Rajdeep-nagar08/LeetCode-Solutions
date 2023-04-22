9dc0827e0a11a8182cdd8c5ad445e8d08f86be1e
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-04-22 22:59:04 +0530

    Time: 808 ms (13.53%), Space: 229.3 MB (6.55%) - LeetHub

---
+++ 1993-operations-on-tree/1993-operations-on-tree.cpp
@@ -0,0 +1,120 @@
+class LockingTree {
+public:
+    
+    unordered_map<int,vector<int>>g;
+    
+    unordered_map<int,int>lckNode; // node => userLocked
+    
+    int n;
+    
+    LockingTree(vector<int>& par) {
+     
+        n=par.size();
+        
+        for(int i=1;i<n;i++){
+            g[par[i]].push_back(i);
+        }
+        
+    }
+    
+    bool lock(int num, int user) {
+        
+        if(lckNode.count(num)==1)
+            return false;
+        
+        lckNode[num]=user;
+        
+        return true;
+        
+    }
+    
+    bool unlock(int num, int user) {
+        
+        
+        if(lckNode.count(num)!=1)
+            return false;
+        
+        if(lckNode[num]!=user)
+            return false;
+        
+        lckNode.erase(num);
+        
+        return true;
+        
+    }
+    
+    bool upgrade(int num, int user) {
+        
+        // upgrade node "num"
+        
+        
+        if(lckNode.count(num)==1)
+            return false;
+        
+        
+        queue<int>q;
+        
+        
+         q.push(num);
+        
+        int c=0;
+        
+        vector<int>v;
+        
+        while(!q.empty()){
+            int x=q.front();
+            q.pop();
+            if(lckNode.count(x)==1){
+                // lckNode.erase(x);
+                v.push_back(x);
+                c++;
+            }
+            
+            for(int y:g[x]){
+                q.push(y);
+            }
+        }
+              
+        if(c==0)
+            return false;
+        
+        
+                
+        vector<bool>vis(n,false);
+
+        
+        for(auto it:lckNode){
+            q.push(it.first);
+            vis[it.first]=true;
+        }
+        
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz--){
+                int x=q.front();
+                q.pop();
+                
+                if(x==num){
+                    return false;
+                }
+                
+                for(int y:g[x]){
+                    if(vis[y]==false){
+                        vis[y]=true;
+                        q.push(y);
+                    }
+                }
+            }
+        }
+    
+        for(int i=0;i<v.size();i++){
+            lckNode.erase(v[i]);
+        }
+                
+        lckNode[num]=user;
+
+        return true;
+        
+    }
+};
+
