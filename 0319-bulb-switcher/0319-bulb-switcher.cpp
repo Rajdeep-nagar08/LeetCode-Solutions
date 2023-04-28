6744bbc2f19a55ab80de460cc0f38c12af4f41a5
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-04-28 11:53:24 +0530

    Time: 0 ms (100.00%), Space: 6 MB (64.29%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        
+        // initially all bulb are off
+        // 1 st round => toggle every bulb
+        // 2nd round => toggle every 2nd bulb
+        // 3rd round=> toggle every 3rd bulb
+        // nth round=> toggle every nth bulb
+        
+        
+        return sqrt(n);
+        
+        
+        
+         
+    }
+};
