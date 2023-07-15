a9ed1d02c24802d886b28f02d79997499c86ff90
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-07-15 21:16:21 +0530

    Time: 318 ms (81.36%), Space: 72.8 MB (71.72%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -13,50 +13,33 @@ select atmost k events
 */
 
 
-int n;
-
 class Solution {
 public:
+    int solve(vector<vector<int>>& ev, int k, int ind, vector<vector<int>> &dp){
+        int n= ev.size();
+        if(k<=0 or ind >=n) 
+            return 0;
+        
+        if(dp[ind][k] != -1)
+            return dp[ind][k];
+        
+        int a= solve(ev, k, ind+1, dp);
+        
+        int i=ind;
+        
+        while(ind<n and ev[ind][0]<=ev[i][1]) 
+            ind++;
+        
+        int b= ev[i][2]+ solve(ev, k-1, ind, dp);
+
+        return dp[i][k]= max(a,b);
+    }
+    
+    
     int maxValue(vector<vector<int>>& events, int k) {
-    
-        n=events.size();
-      
-        
-          vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        
-        sort(events.begin(),events.end());
-        
-//         for(int i=0;i<n;i++){
-//             cout<<events[i][0]<<" "<<events[i][1]<<" "<<events[i][2]<<endl;
-//         }
-        
-        
-        return find(0,events,k,dp);
-        
-    }
-    
-    
-    int find(int i,vector<vector<int>>&events,int k,vector<vector<int>>&dp){
-        
-          if(i>=n || k<=0)
-              return 0;
-        
-        if(dp[i][k]!=-1)
-            return dp[i][k];
-        
-        int x=0,y=0;
-        
-        x=find(i+1,events,k,dp);
-    
-            vector<int>v1={events[i][1],INT_MAX,INT_MAX};
-            
-            int idx=upper_bound(events.begin()+i,events.end(),v1)-events.begin();
-            
-            y=events[i][2]+find(idx,events,k-1,dp);
-            
-    
-        
-        return dp[i][k]=max(x,y);
-        
-    }
+        sort(events.begin(), events.end());
+        int n= events.size();
+        vector<vector<int>> dp(n+1, vector<int> (k+1, -1));
+        return solve(events, k, 0 , dp);
+    }
 };
