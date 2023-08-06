8e60fedbcabe41adb75251be23c23411eea16def
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-08-06 18:35:59 +0530

    Time: 1555 ms (24.74%), Space: 0B (100.00%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+
+# AVG= SUM(true values)/COUNT(*)
+
+
+
+select ROUND((SUM(CASE WHEN order_date=customer_pref_delivery_date then 1 else 0 
+    
+                  end)*100)/COUNT(DISTINCT customer_id),2)
+              
+as immediate_percentage from Delivery where (customer_id, order_date) in
+
+(select customer_id, min(order_date) from Delivery group by 
+ 
+ customer_id);
+
+
