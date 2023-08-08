2755f0850fbfd3fee72bdf3a50b9de45b1484375
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-08-09 01:53:40 +0530

    Time: 1593 ms (24.59%), Space: 0B (100.00%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -2,17 +2,14 @@
 
 SELECT t1.employee_id, t1.name, 
        
-SUM(case when t1.employee_id = t2.reports_to then 1 else 0 end) 
-as reports_count, 
-       
-ROUND(AVG(t2.age)) as average_age
-       
-from Employees as t1
+count(t2.reports_to) as reports_count,
 
-left join Employees as t2 on t1.employee_id = t2.reports_to
+round(avg(t2.age)) as average_age
 
-group by t1.employee_id, t1.name
+from Employees t1 join Employees t2
 
-having reports_count>0
+on t1.employee_id=t2.reports_to
+
+group by t1.employee_id
 
 order by t1.employee_id;
