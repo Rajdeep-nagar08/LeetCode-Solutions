05abeb6ee54b6a50bcb318b16f82520652242b92
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-08-26 15:14:38 +0530

    Time: 240 ms (82.72%), Space: 207 MB (7.81%) - LeetHub

--- 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.cpp
+++ 2771-longest-non-decreasing-subarray-from-two-arrays/2771-longest-non-decreasing-subarray-from-two-arrays.cpp
@@ -1,86 +1,70 @@
 
-int dp[100001][3];  
-
-// dp[i][x] = longest increasing subarray if we need to chosse ith no from either of the array and (i-1)th no. was chosen from xth array 
 
 
-// concept similar to longest common subarray
 
-// ans= max(dp)
+    int dp[100001][3];
 
-int ans=0;
+
+int finalans=0;
 
 class Solution {
 public:
     int maxNonDecreasingLength(vector<int>& nums1, vector<int>& nums2) {
         
-        ans=0;
+        finalans=0;
         
-        int n=nums1.size();
+         int n=nums1.size();
+        
+        memset(dp, -1, sizeof(dp));
+        
+        helper(0, 0, nums1, nums2);
         
         
-        memset(dp,-1,sizeof(dp));
-        
-        
-        find(1,0,nums1,nums2);
-        
-        find(1,1,nums1,nums2);
-        
-    
-        return ans+1;
+        return finalans;
+
         
     }
     
-    int find(int i,int x,vector<int>&nums1,vector<int>&nums2){
-        if(i>=nums1.size())
-            return 0;
+    
+    int helper(int i, int prev, vector<int> &nums1, vector<int> &nums2){
         
-        if(dp[i][x]!=-1)
-            return dp[i][x];
+        if(i==nums1.size()) return 0;
         
+        if(dp[i][prev] != -1) 
+            return dp[i][prev];
         
-        int ans1=0;
+        int ans = 0;
         
-        int ans2=0;
-        
-        
-        
-        if(x==0){  // (i-1)th no was chosen from nums1
-            
-            if(nums1[i]>=nums1[i-1]){
-                ans1=1+find(i+1,0,nums1,nums2);
-            }
-           
-            
-            if(nums2[i]>=nums1[i-1]){
-                ans2=1+find(i+1,1,nums1,nums2);
-            }
-           
-            
+        if(prev==0){
+            helper(i+1, 0, nums1, nums2);
+            int f = 1+helper(i+1, 1, nums1, nums2);
+            int s = 1+helper(i+1, 2, nums1, nums2);
+            ans = max({ans, f, s});
         }
-        else{  //  // (i-1)th no was chosen from nums2
+        else if(prev==1){
+            helper(i+1, 0, nums1, nums2);
+            int f=0, s=0;
+            if(nums1[i]>=nums1[i-1]) f = 1+helper(i+1, 1, nums1, nums2);
+            if(nums2[i]>=nums1[i-1]) s = 1+helper(i+1, 2, nums1, nums2);
             
+            ans=max({ans, f, s});
+        }
+        else{
+            helper(i+1, 0, nums1, nums2);
+            int f=0, s=0;
+            if(nums1[i]>=nums2[i-1]) f = 1+helper(i+1, 1, nums1, nums2);
+            if(nums2[i]>=nums2[i-1]) s = 1+helper(i+1, 2, nums1, nums2);
             
-            if(nums1[i]>=nums2[i-1]){
-                ans1=1+find(i+1,0,nums1,nums2);
-            }
-            
-            
-            if(nums2[i]>=nums2[i-1]){
-                ans2=1+find(i+1,1,nums1,nums2);
-            }
-           
-            
+            ans=max({ans, f, s});
         }
         
-        dp[i][x]=max(ans1,ans2);
+        finalans=max(finalans,ans);
         
-        find(i+1,0,nums1,nums2);
+        return dp[i][prev]  = ans;
         
-        find(i+1,1,nums1,nums2);
-       
-        ans=max(ans,dp[i][x]);
         
-        return dp[i][x];
     }
+
+    
+    
 };
