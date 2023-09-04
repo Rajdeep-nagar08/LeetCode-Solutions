4adf080afdc8207cc3a27d74c9a8afa2f987d9b8
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-09-04 15:10:30 +0530

    Time: 10 ms (80.11%), Space: 19.8 MB (71.86%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,89 +1,48 @@
-
-/*
-https://leetcode.com/problems/trapping-rain-water/discuss/153992/Java-O(n)-time-and-O(1)-space-(with-explanations).
-
-
-O(n) time, O(1) space
-
-water stored on top of any building = 
-
-min(maxL,maxR)-height of building
-
-
-if min(maxL,maxR) < height of building, water stored on top of it=0
-
-
-[3(maxL),1(l),0,2,4,6,2,0,5(r,maxR)]
-
-maxL > l < r = maxR : water stored on top of 1: (maxL-l) = 2, l++
-
-
-[3(maxL),1,0(l),2,4,6,2,0,5(r,maxR)]
-
-maxL > l < r = maxR : water stored on top of 0: (maxL-l) = 3, l++
-
-
-[3(maxL),1,0,2(l),4,6,2,0,5(r,maxR)]
-
-maxL > l < r = maxR : water stored on top of 2: (maxL-l) = 1, l++
-
-
-[3(maxL),1,0,2,4(l),6,2,0,5(r,maxR)]
-
-Now maxL < l < r = maxR, update maxL, No water stored on top of l or 3, l++
-
-[3,1,0,2,4(maxL),6(l),2,0,5(r,maxR)]
-
-maxL < (l > r = maxR), No water stored on top of 6 and 5 as well
-
-r--, update maxL
-
-[3,1,0,2,4,6(maxL,l),2,0(r),5(maxR)]
-
-maxL = l > r < maxR
-
-water stored on top of r (0) : (maxR-r) = 5, r--
-
-
-[3,1,0,2,4,6(maxL,l),2(r),0,5(maxR)]
-
-maxL = l > r < maxR
-
-water stored on top of r (2) : (maxR-r) = 3, r--
-
-[3,1,0,2,4,6(maxL,l,r),2,0,5(maxR)]
-
-maxL=l=r<maxR
-
-stop (l==r)
-
-
-*/
-
 class Solution {
 public:
-    int trap(vector<int>& height) {
+    int trap(vector<int>& ht) {
         
-      int n=height.size();
-      vector<int>pref1(n);
-       pref1[0]=height[0];
-      
-      for(int i=1;i<n;i++)
-        pref1[i]=max(pref1[i-1],height[i]);
-      
-      vector<int>pref2(n);
-      pref2[n-1]=height[n-1];
-      for(int i=n-2;i>=0;i--)
-        pref2[i]=max(pref2[i+1],height[i]);
-      
-      
-      long long int ans=0;
-      for(int i=0;i<n;i++){
-        if(height[i]==pref1[i] || height[i]==pref2[i])
-          continue;
         
-        ans=ans + min(pref1[i],pref2[i])-height[i];
-      }
+        int n=ht.size();
+        
+        
+        int ans=0;
+        
+        int mxL=ht[0],i=1;
+        
+        int mxR=ht[n-1],j=n-2;
+        
+        while(i<=j){
+            
+            
+            if(mxL<mxR){
+                
+                if(ht[i]<mxL){
+                    ans+=mxL-ht[i];
+                }
+                else{
+                    mxL=ht[i];
+                }
+                
+                i++;
+                
+            }
+            
+            else if(mxL>=mxR){
+                
+                if(ht[j]<mxR){
+                    ans+=mxR-ht[j];
+                }
+                else{
+                    mxR=ht[j];
+                }
+                
+                j--;
+                
+            }
+            
+        }
+        
         return ans;
     }
 };
