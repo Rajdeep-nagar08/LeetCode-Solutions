a52ee23135a56e9176e81d767a85afebce954aa3
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-09-04 15:18:12 +0530

    Time: 17 ms (32.88%), Space: 19.7 MB (71.86%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,3 +1,58 @@
+
+/*
+https://leetcode.com/problems/trapping-rain-water/discuss/153992/Java-O(n)-time-and-O(1)-space-(with-explanations).
+
+
+O(n) time, O(1) space
+
+water stored on top of any building = 
+
+min(maxL,maxR)-height of building
+
+
+if min(maxL,maxR) < height of building, water stored on top of it=0
+
+
+[3(maxL),1(l),0,2,4,6,2,0(r),5(maxR)]
+
+maxL < maxR and (maxL>l) : water stored on top of 1: (maxL-l) = 2, l++
+
+
+[3(maxL),1,0(l),2,4,6,2,0(r),5(maxR)]
+
+maxL< maxR and (maxL>l) : water stored on top of 0: (maxL-l) = 3, l++
+
+
+[3(maxL),1,0,2(l),4,6,2,0(r),5(maxR)]
+
+maxL < maxR and (maxL > l) : water stored on top of 2: (maxL-l) = 1, l++
+
+
+[3(maxL),1,0,2,4(l),6,2,0(r),5(maxR)]
+
+Now maxL < maxR but l>maxL, update maxL, No water stored on top of l or 3, l++
+
+
+[3,1,0,2,4(maxL),6(l),2,0(r),5(maxR)]
+
+maxL < maxR, but l>maxL  No water stored on top of 6 
+
+update maxL
+
+[3,1,0,2,4,6(maxL),2(l),0(r),5(maxR)]
+
+maxL > maxR, and r<maxR,: water stores on top of r =0: (maxR-r) = 5-0=0
+
+
+[3,1,0,2,4,6(maxL),2(l,r),0,5(maxR)]
+
+maxL > maxR, and r<maxR, water stored on top of r=2: (maxR-r)= 5-2= 3
+
+
+
+
+*/
+
 class Solution {
 public:
     int trap(vector<int>& ht) {
