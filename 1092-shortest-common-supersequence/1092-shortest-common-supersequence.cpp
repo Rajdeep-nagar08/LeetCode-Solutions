e981dbc19712ef65b7446df23e91d6ae90efb2da
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-09-06 00:53:39 +0530

    Time: 21 ms (67.14%), Space: 10.4 MB (99.84%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,8 +1,20 @@
 /*
 
+  LCS:
+      dp[i][j]=length of lcs btw two strings of length i , j
+      if(x[i]==y[j])
+      dp[i][j]= 1+dp[i-1][j-1]
+      else
+      dp[i][j]= max(dp[i-1][j],dp[i][j-1])
+      length of shortest common subseq= (l1 + l2 - lcs(l1,l2))
+      
+      
+
 whenever the question asks you to find the objects chosen instead of the final value you get when you choose these objects (like the indices selected in this question)
 
 we can do the DP for finding the value (like the maximum sum in this case) and do a DFS for retracing the steps.
+     
+      
 
 */
    
@@ -17,11 +29,49 @@ public:
         
         memset(dp,-1,sizeof(dp));
         
-         find(0,0,str1,str2);
+        // finding length of LCS
+        find(0,0,str1,str2);
         
-         dfs(0,0,str1,str2);
+        int n=str1.size();
         
-       // reverse(ans.begin(),ans.end());
+        int m=str2.size();
+        
+        int i=0,j=0;
+        
+        string ans="";
+        
+        while(i<n && j<m){
+            
+            
+        if(str1[i]==str2[j]){
+            
+            ans.push_back(str1[i]);
+            
+            i++,j++;
+        }
+            
+    else if(dp[i+1][j]>dp[i][j+1]){
+         ans.push_back(str1[i]);
+        i++;
+        }
+            else{
+                
+                ans.push_back(str2[j]);
+                j++;
+            }
+            
+        }
+        
+           while(i<str1.size()){
+                ans.push_back(str1[i]);
+                i++;
+            }
+            
+            
+            while(j<str2.size()){
+                ans.push_back(str2[j]);
+                j++;
+            }
         
         return ans;
         
@@ -42,56 +92,5 @@ public:
     }
     
     
-    void dfs(int i,int j,string &str1, string &str2){
-        
-        if(i>=str1.size() || j>=str2.size()){
-            
-            while(i<str1.size()){
-                ans.push_back(str1[i]);
-                i++;
-            }
-            
-            
-            while(j<str2.size()){
-                ans.push_back(str2[j]);
-                j++;
-            }
-            
-            return ;
-            
-        }
-               
-        int ch1=0,ch2=0;
-
-        if(str1[i]==str2[j]){
-            
-            ans.push_back(str1[i]);
-            
-            dfs(i+1,j+1,str1,str2);
-        }
-        
-        
-        else{
-            
-             ch1=find(i+1,j,str1,str2);
-            
-             ch2=find(i,j+1,str1,str2);
-            
-            if(ch1>=ch2){
-                ans.push_back(str1[i]);
-                
-                dfs(i+1,j,str1,str2);
-            }
-
-            else{
-                ans.push_back(str2[j]);
-                
-                dfs(i,j+1,str1,str2);
-            }
-            
-        }
-        
-        
-        
-    }
+   
 };
