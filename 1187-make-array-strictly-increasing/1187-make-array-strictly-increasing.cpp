f86fd6748e30270c073d3650d6819d1ff69d5489
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-09-14 12:02:19 +0530

    Time: 325 ms (73.42%), Space: 70.4 MB (66.44%) - LeetHub

--- 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -6,10 +6,22 @@ public:
       
       unordered_map<int,int>mp{{-10,0}};   
       
-        /* to store information of valid subarrays till now,As:-
+        /* 
+        
+        at each arr1[i] we have two choices
+        
+        either include it (if including it makes subarray increasing)
+        
+        or
+        
+        replace it with just greater no. from array2
+        
+        to store information of valid subarrays till now,As:-
       (last no of  valid subarray ->no of operations till now or no of digits of arr2 used till now)
       e.g valid subarray x1 < x2 < y3 < x4 < y5 where (y3 and y5 taken from arr2)
           for this store {y5 , 2}
+          
+          
           
       */
       
@@ -21,7 +33,7 @@ public:
           
           if(it.first < no1){   // no need to do selection from array2
             
-            if(temp.find(no1)==temp.end()) //not included till now so include it
+            if(temp.find(no1)==temp.end()) 
                temp[no1]=it.second;
             else
                temp[no1]=min(temp[no1],it.second);
@@ -36,7 +48,6 @@ public:
               
             if(temp.find(*no2)==temp.end())
                temp[*no2]= it.second +1;   
-                // as we are using arr2 no, so count of operations increase
             else
                temp[*no2]=min(temp[*no2],it.second +1);
           }
