ea10888798eb9f13ac5d949f7053e7223ca76599
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-09-23 14:48:06 +0530

    Time: 195 ms (35.15%), Space: 18 MB (47.04%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,10 +1,66 @@
+int dp[1002][1002];
+
 class Solution {
 public:
+    int longestStrChain(vector<string>& words) {
+        
+        
+        int n=words.size();
+        
+        memset(dp,-1,sizeof(dp));
+        
+        /*
+        
+        longest possible word chain
+        
+        
+        dp[i] = longest possible word chain in [i....n-1]  starting from index i
+        
+        */
+        
+        sort(words.begin(),words.end(),[](string &s1, string &s2){
+            return s1.size()<s2.size();
+            
+        });
+        
+                
+        // making 1 based indexing to avoid passing prev=-1 in starting
+        
+        return find(1,0,words);
+        
+        
+    }
     
+    int find(int i, int prev, vector<string>&words){
+        if(i>=words.size()+1)
+            return 0;
+        
+        
+        if(dp[i][prev]!=-1)
+            return dp[i][prev];
+        
+       int ans1=0,ans2=0;
+        
+        ans1=find(i+1,prev,words);
+
+       if(prev==0){
+           
+           ans2=1+find(i+1,i,words);
+       }
+        
+        else if(words[prev-1].size()+1 ==words[i-1].size() && (isValid(words[i-1],words[prev-1])))
+                ans2=1+find(i+1,i,words);
+                
+        
+        
+        return dp[i][prev]=max(ans1,ans2);
+        
+        
+    }
     
-    bool compare(string &x, string& y)
-    {
-        if(x.size()!=y.size()+1)
+    bool isValid(string x, string y){
+        
+         if(x.size()!=y.size()+1)
         {
             return false;
         }
@@ -32,33 +88,6 @@ public:
         }
         
         return false;
-        
-    }
-    static bool comp(string &a, string &b)
-    {
-        return a.size()<b.size();
-    }
-    int longestStrChain(vector<string>& words)
-    {
-        
-        sort(words.begin(),words.end(),comp);
-        vector<int>dp(words.size(),1);
-        
-        int ans=0;
-        for(int i=0;i<words.size();i++)
-        {
-            for(int prev=0;prev<i;prev++)
-            {
-                if(compare(words[i],words[prev]) &&  dp[prev]+1>dp[i])
-                {
-                    dp[i]=dp[prev]+1;
-                }
-            }
-            
-            ans=max(ans,dp[i]);
-        }
-        
-        return ans;
         
     }
 };
