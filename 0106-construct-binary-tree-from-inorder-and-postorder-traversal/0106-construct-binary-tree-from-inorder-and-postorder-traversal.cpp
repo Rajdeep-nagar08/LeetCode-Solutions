5b500141be4f86f118f9f8f61a687054b0915ce7
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-10-15 20:16:21 +0530

    Time: 33 ms (14.11%), Space: 26.2 MB (79.86%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -10,46 +10,24 @@
  * };
  */
 
-/*
-inorder: left,root,right
-postorder: left,right,root
-*/
 
 class Solution {
-  int k;
 public:
+    TreeNode* fun(vector<int>& inorder, vector<int>& postorder,int &rootptr,int start,int end){
+        if(start>end) return NULL;
+        int inptr=start;
+        while(postorder[rootptr]!=inorder[inptr]) inptr++;
+        rootptr--;
+        
+        TreeNode* mynode=new TreeNode(inorder[inptr]);
+        mynode->right=fun(inorder,postorder,rootptr,inptr+1,end);
+        mynode->left=fun(inorder,postorder,rootptr,start,inptr-1);
+        
+        
+        return mynode;
+    }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-      k=postorder.size()-1;
-
-      return buildT(inorder,postorder,0,postorder.size()-1);
+        int rootptr=inorder.size()-1;
+        return fun(inorder,postorder,rootptr,0,inorder.size()-1);
     }
-     
-    TreeNode* buildT(vector<int>&inorder,vector<int>&postorder,int start,int end){
-  
-      if(start>end)
-        return nullptr;
-      
-      int curr=postorder[k];
-       k--;
-      
-      TreeNode* root=new TreeNode(curr);
-      if(start==end)
-        return root;
-     int pos=search(inorder,start,end,curr);
-      root->right=buildT(inorder,postorder,pos+1,end);
-
-      root->left=buildT(inorder,postorder,start,pos-1);
-
-      return root;
-    }
-  int search(vector<int>& inorder,int start,int end,int key){
-    for(int i=start;i<=end;i++){
-      if(inorder[i]==key)
-        return i;
-    }
-      return -1;
-  }
-     
 };
-
-
