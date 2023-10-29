01cf95140714354098f55cfabd981c2e5de57ae9
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-10-29 09:38:27 +0530

    Time: 488 ms (88.89%), Space: 206.8 MB (66.67%) - LeetHub

---
+++ 2920-maximum-points-after-collecting-coins-from-all-nodes/2920-maximum-points-after-collecting-coins-from-all-nodes.cpp
@@ -0,0 +1,59 @@
+// unordered_map<int,unordered_map<int,int>>dp;
+
+int dp[100001][15];
+
+class Solution {
+public:
+    int maximumPoints(vector<vector<int>>& edges, vector<int>& coins, int k) {
+       
+        // dp.clear();
+        
+        memset(dp,-1,sizeof(dp));
+        
+        int n=coins.size();
+        
+        vector<int>g[n];
+        
+        for(auto it:edges){
+            g[it[0]].push_back(it[1]);
+            g[it[1]].push_back(it[0]);
+        }
+        
+        
+        int half=0; 
+        
+        return max(coins[0]-k+dfs(0,-1,half,g,k,coins),coins[0]/2+dfs(0,-1,half+1,g,k,coins));
+    }
+    
+    
+    int dfs(int node, int par, int half, vector<int>g[], int k, vector<int>&coins){
+        
+        int ans=0;
+        
+        if(dp[node][half]!=-1)
+            return dp[node][half];
+        
+        for(int y:g[node]){
+            if(y!=par){
+            int points= coins[y]/(pow(2,half));
+                
+            if(half==14){
+            
+             return 0;
+                
+            }
+                
+                else{
+                    
+                                
+       ans+= max(points-k+dfs(y,node,half,g,k, coins),(points/2)+dfs(y,node,half+1,g,k, coins));
+
+                    
+                }
+            }
+            
+        }
+        
+        return dp[node][half]=ans;
+    }
+};
