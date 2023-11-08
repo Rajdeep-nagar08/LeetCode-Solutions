ac6b40a3dc56cd2645e2fdd5321792c5cfd4a47b
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-11-08 16:39:42 +0530

    Time: 766 ms (21.93%), Space: 42.8 MB (71.14%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,19 +1,61 @@
+#define lln long long int
+
+unordered_map<char,unordered_map<int,int>>mp; // target[ind2] => {ind1, count}
+
 class Solution {
 public:
-    int numWays(vector<string>& A, string target) {
-        long mod = 1e9+7, M = A.size(), L = A[0].size(), N = target.size();
-        if (L < N) return 0;
-        vector<array<long, 26>> cnt(L, array<long, 26>());
-        for (int i = 0; i < L; ++i) {
-            for (int j = 0; j < M; ++j) cnt[i][A[j][i] - 'a']++;
-        }
-        vector<vector<int>> dp(L + 1, vector<int>(N + 1));
-        for (int i = 0; i < L; ++i) {
-            dp[i][0] = 1;
-            for (int j = 0; j <= i && j < N; ++j) {
-                dp[i + 1][j + 1] = ((cnt[i][target[j] - 'a'] * dp[i][j]) % mod + dp[i][j + 1]) % mod;
+    
+long long mod=1e9+7;
+    
+lln dp[1001][1001];
+    
+    lln func(int ind1,int ind2,vector<string>& words, string &target){
+        
+        if(ind2==target.size()) 
+            return 1;
+        
+        if(ind1>=words[0].size()) 
+            return 0;
+        
+        if(dp[ind1][ind2]!=-1) 
+            return dp[ind1][ind2];
+        
+        lln res=0;
+        
+        lln count=mp[target[ind2]][ind1];
+      
+         if(count>0){
+         count%=mod;
+         res+=count*func(ind1+1,ind2+1,words,target)%mod;
+
+         }
+        
+        res+=func(ind1+1,ind2,words,target)%mod;
+        
+        return dp[ind1][ind2]=res%mod;
+        
+    }
+    
+    int numWays(vector<string>& words, string target) {
+        
+        memset(dp,-1,sizeof(dp));
+        
+        mp.clear();
+        
+        for(int ind2=0;ind2<target.size();ind2++){    // O(26)
+            if(mp.count(target[ind2])==1)
+                continue;
+            
+            for(int ind1=0;ind1<words[0].size();ind1++){  // O(1000)
+                for(int i=0;i<words.size();i++){  // O(1000)
+                    if(words[i][ind1]==target[ind2])
+                    mp[target[ind2]][ind1]++;
+                }
             }
         }
-        return dp[L][N];
+        
+        
+        
+        return func(0,0,words,target);   // T.C = O(26*1000*1000)
     }
 };
