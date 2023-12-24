ea406da4a0823a160f2374d2c33eae8a90703c48
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2023-12-24 22:55:10 +0530

    Time: 16 ms (79.15%), Space: 13.5 MB (67.30%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,67 +1,26 @@
 class Solution {
 public:
+    int dfs(int i, int j,vector<vector<int>> &grid,int n,int m){
+        if(i>=n)
+            return j;
+        
+        
+        if(grid[i][j]== 1 and j+1< m and grid[i][j+1]== 1) 
+            return dfs(i+1,j+1,grid,n,m);
+        
+        else if(grid[i][j]==-1 and j-1>=0 and grid[i][j-1]==-1) 
+            return dfs(i+1,j-1,grid,n,m);
+        
+            return -1;
+    }
+    
     vector<int> findBall(vector<vector<int>>& grid) {
-        
-        
         int n=grid.size();
-        
         int m=grid[0].size();
-        
-        // ans[i] = column in which ball at column i lands down
-        
-        vector<int>ans(m);
-        
-        for(int j=0;j<m;j++){
-                        
-            
-            if(j==0 && (grid[0][j]==-1)){
-                ans[j]=-1;
-                continue;
-            }
-            
-            if(j==m-1 && (grid[0][j]==1)){
-                ans[j]=-1;
-                continue;
-            }
-            
-            bool flag=true;
-            
-            int r=0;
-
-            int c=j;
-            
-            while(r>=0 && r<n && c>=0 && c<m){
-                if(grid[r][c]==1 && (c+1<m)){
-                    if(grid[r][c+1]==1){
-                        r++,c++;
-                    }
-                    else{
-                        flag=false;
-                        break;
-                    }
-                }
-                else{
-                    if((c-1>=0) && grid[r][c-1]==-1){
-                        r++,c--;
-                    }
-                    else{
-                        flag=false;
-                        break;
-                    }
-                }
-                
-               
-            }
-            
-            if(flag==false)
-                ans[j]=-1;
-            else
-                ans[j]=c;
-            
+        vector<int> col(m);
+        for(int i=0;i<m;i++){
+            col[i]=dfs(0,i,grid,n,m);
         }
-        
-        
-        return ans;
-        
+        return col;                                                                                
     }
 };
