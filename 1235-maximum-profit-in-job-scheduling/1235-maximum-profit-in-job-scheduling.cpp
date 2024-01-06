85c519cbf7079f7b66dfd2afb590442e53d8a656
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2024-01-06 15:20:55 +0530

    Time: 282 ms (19.70%), Space: 105.8 MB (7.28%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,74 +1,71 @@
- /*
-     
-     maximum profit ?
-    
-     for a paticular event, we have two choices either choose it or not choose it
-     
-     as we have choices we can think about dp
-     
-     if we are choosing int event [x,y]
-     
-     then next events starting time should be >=y
-     
-     
-     dp[i]= max profit we can make by choosing events from [i....n-1] events
-     
-     
-     ans=dp[0]
-    
-      
-     */
 
 
 int dp[50001];
 
 class Solution {
 public:
-    int jobScheduling(vector<int>& st, vector<int>& et, vector<int>& prof) {
+    int jobScheduling(vector<int>& start, vector<int>& end, vector<int>& profit) {
         
-        memset(dp,-1,sizeof(dp));
         
-        vector<vector<int>>v(prof.size());
+        /*
+        needs to maximize the profit
         
-        for(int i=0;i<prof.size();i++){
-            v[i]={st[i],et[i],prof[i]};
+        for eachg job, we have two choices, either do it or not do it
+        
+        
+        => recursion => dp
+    
+    
+    
+    if any job ends at time t
+    
+    next job should start from time >=t
+    
+    
+    sort jobs as per the start time
+    
+        
+        */
+        
+        
+        int n=start.size();
+        
+        vector<vector<int>>v;
+        
+        for(int i=0;i<n;i++){
+            v.push_back({start[i],end[i],profit[i]});
         }
         
         sort(v.begin(),v.end());
         
-        return find(0,v);
+        memset(dp,-1,sizeof(dp));
         
-     
+        
+   return find(0,v);
+        
+        
     }
     
-    int find(int i,vector<vector<int>>&v){
-        if(i>=v.size())
+    int find(int i, vector<vector<int>>&v){
+        
+        if(i>=v.size()){
             return 0;
+        }
         
         if(dp[i]!=-1)
             return dp[i];
         
-        int ans1=0;
+        int ans1=find(i+1,v);
         
-        int ans2=0;
-        
-        // not choosing this event
-        
-        ans1+=find(i+1,v);
-        
-        
-        // choosing this event
-        
-        int s=v[i][0];
+        int ans2=v[i][2];
         
         int e=v[i][1];
         
         vector<int>v1={e,INT_MIN,INT_MIN};
         
-        int i1=lower_bound(v.begin(),v.end(),v1)-v.begin();
+        int idx=lower_bound(v.begin(),v.end(),v1)-v.begin();
         
-        ans2+=v[i][2]+find(i1,v);
-        
+        ans2+=find(idx,v);
         
         return dp[i]=max(ans1,ans2);
         
