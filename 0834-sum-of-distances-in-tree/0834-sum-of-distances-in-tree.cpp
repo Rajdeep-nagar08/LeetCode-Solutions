fa4b6bcfe4101d1838544a42f35740ca2b7d7200
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2024-01-06 15:04:19 +0530

    Time: 290 ms (33.39%), Space: 104.3 MB (56.56%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,75 +1,88 @@
 class Solution {
 public:
-    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& e) {
+    
+    vector<int>dist;
+    
+    vector<int>cnt;
+    
+    vector<int>ans;
+    
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
         
-        /*
-        
-        dist[x] = distance of x from all other nodes
-        =
-        distance of x from nodes under x + disance of x from nodes not under x
-        
-        */
         
         vector<int>g[n];
         
-        for(auto it:e){
+        for(auto it:edges){
             g[it[0]].push_back(it[1]);
             g[it[1]].push_back(it[0]);
         }
         
-        vector<int>nodes(n,0);  // nodes[x]= no of nodes under x
-        vector<int>ans1(n,0);  // ans1[x]= sum of distances of all nodes under x from x, clearly for x---->y, ans1[x]=nodes[y]+ans1[y]
+        dist.resize(n,0);
         
-        vector<int>ans2(n,0);  // ans2[x]=sum of diastances of all nodes from x = ans1[x]+(sum of distances of all nodes that are not under x from x + (n-nodes[x])) = ans1[x]+(partial ans for x + (n-nodes[x]))
+        cnt.resize(n,0);
         
-        // note : partial ans for root node=0
-        
-    //Now for x----->y , partial ans for y=sum of distances of all nodes that are not under y from node y= ans2[x]-(ans1[x]+nodes[y])
+        ans.resize(n,0);
         
         
-        dfs1(0,-1,g,nodes);
-        dfs2(0,-1,g,nodes,ans1);
-        dfs3(0,-1,0,g,nodes,ans1,ans2);
+        dfs1(0,-1,g);
         
-        return ans2;
+//         for(int x:dist){
+//             cout<<x<<" ";
+//         }
+        
+        dfs2(0,-1,g);
+        
+        return ans;
+        
+        
+        
         
     }
     
-    void dfs1(int x,int par,vector<int>g[],vector<int>&nodes){
-      
-        int count=1;
+    int dfs1(int node, int par, vector<int>g[]){
         
-        for(int y:g[x]){
-            if(y!=par){
-              dfs1(y,x,g,nodes);
-              count+=(nodes[y]);
-            }
+        int c;
+        
+        for(int x:g[node]){
+           if(x!=par){
+               c=1+dfs1(x,node,g);
+               cnt[node]+=c;
+               dist[node]+=c+dist[x];
+               
+           } 
         }
-        nodes[x]=count; 
-    } 
     
-    void dfs2(int x,int par,vector<int>g[],vector<int>&nodes,vector<int>&ans1){
-      
-        int d1=0;
-        
-        for(int y:g[x]){
-            if(y!=par){
-              dfs2(y,x,g,nodes,ans1);
-              d1+=(nodes[y]+ans1[y]);
-            }
-        }
-        ans1[x]=d1; 
-    } 
+        return cnt[node];
+    }
     
     
-    void dfs3(int x,int par,int partial_ans,vector<int>g[],vector<int>&nodes,vector<int>&ans1,vector<int>&ans2){
-      
-    ans2[x]=ans1[x] + (partial_ans+((int)ans2.size()-nodes[x]));
+    void dfs2(int node, int par,vector<int>g[]){
         
-        for(int y:g[x]){
-            if(y!=par){
-              dfs3(y,x,ans2[x]-ans1[y]-nodes[y],g,nodes,ans1,ans2);
+    // answer from up = total ans of parent node - contribution from this node subtree + (no of nodes from upper side)
+        
+    // answer from up = ans[par] - contribution from this node subtree + (cnt[0]-cnt[node])
+        
+   // contribution from this node subtree = dist[node]+cnt[node]+1
+        
+  // answer from up = ans[par] - (dist[node]+cnt[node]+1) + (cnt[0]-cnt[node])
+        
+        
+        int ansUp= 0;
+        
+        if(par!=-1){
+            ansUp+=ans[par]-(dist[node]+cnt[node]+1)+ (cnt[0]-cnt[node]);
+        }
+        
+        ans[node]=ansUp+dist[node];
+            
+        for(int x:g[node]){
+            
+            if(x!=par){
+                
+               dfs2(x,node,g); 
+                
             }
         }
-    } 
+        
+    }
 };
