081024caf58653d7cc049938aa6365eb2ef1d6bd
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2024-07-14 23:31:33 +0530

    Time: 825 ms (59.56%), Space: 20.7 MB (26.55%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,117 +1,94 @@
-int dx[4]={1,-1,0,0};
-int dy[4]={0,0,1,-1};
+
+struct TrieNode{
+    TrieNode* children[26];
+    bool end;
+};
+
+TrieNode* root;
+
+void insert(string str){
+    TrieNode* temp=root;
+    for(char ch: str){
+        int idx=ch-'a';
+        if(temp->children[idx]==NULL){
+            temp->children[idx]=new TrieNode();
+        }
+        temp=temp->children[idx];   
+    }
+    temp->end=true;
+}
+
+bool check(string str){
+    TrieNode* temp=root;
+    for(char ch: str){
+        int idx=ch-'a';
+        if(temp->children[idx]==NULL)
+            return false;
+        temp=temp->children[idx];
+    }
+    return temp->end;
+}
+
+
+vector<string>ans;
 
 class Solution {
 public:
-    int n,m;
-    
-    
-    unordered_map<string,int>mp;
-    
-    vector<string>ans;
-
-    
-struct TrieNode {
-
-int wc;
-
-struct TrieNode* children[26];
-
- };
-
-
-struct TrieNode* getNode()
-{
-
-    struct TrieNode* pNode = new TrieNode;
- 
-   
- for (int i = 0; i < 26; i++){
-        pNode->children[i] = NULL;
-        }
-
- 
-        pNode->wc= 0;
-
-    return pNode;
-    
-}
-    
-struct TrieNode *root = getNode();
-
-void insert(struct TrieNode* root, string key)
-{
-
-    struct TrieNode* pCrawl = root;
- 
-
-    for (int i = 0; i < key.length(); i++) {
-
-        int index = key[i] - 'a';
-
-        if (!pCrawl->children[index])
-        
-       pCrawl->children[index] = getNode();
- 
-
-        pCrawl = pCrawl->children[index];
-     
-    }
-         
-    pCrawl->wc +=1; //not write pCrawl->wc+=1
-
-}
- 
-
-
-    
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+       
+        root=new TrieNode();
+        ans.clear();
         
-        n=board.size();
-        
-        m=board[0].size();
-        
-        
-        for(string str:words){
-            insert(root,str);
+        for(string str: words){
+            insert(str);
         }
-    
+        
+                
+        int n=board.size();
+        
+        int m=board[0].size();
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                string str="";
                 vector<vector<bool>>vis(n,vector<bool>(m,false));
-                 string str="";
-                str+=board[i][j];
-                if(root->children[board[i][j]-'a']!=NULL)
-                vis[i][j]=true;
-                dfs(i,j,str,root->children[board[i][j]-'a'],vis,board);
-                }
+                TrieNode* temp=root;
+                find(i,j,str,board, vis,temp); 
             }
-        
+        }
         return ans;
-    
     }
     
+    int dx[4]={1,-1,0,0};
     
-    void dfs(int i,int j, string str,TrieNode* root,vector<vector<bool>>&vis,vector<vector<char>>&board){
+    int dy[4]={0,0,1,-1};
+    
+void find(int i, int j, string& str, vector<vector<char>>& board, vector<vector<bool>>& vis, TrieNode* temp)
+{
+        if(temp->children[board[i][j]-'a']==NULL)
+            return;
         
-        if(!root)
-            return ;
-        
-        if(root->wc){
+        temp=temp->children[board[i][j]-'a'];
+    
+        vis[i][j]=true;
+    
+        str.push_back(board[i][j]);
+
+        if(temp->end==true){
             ans.push_back(str);
-            root->wc=0;
+            temp->end=false;
         }
-        
-        for(int k=0;k<4;k++){
-           int x1=i+dx[k];
-            
-            int y1=j+dy[k];
-            
-    if(x1>=0 && y1>=0 && x1<n && y1<m && (vis[x1][y1]==false) && (root->children[board[x1][y1]-'a']!=NULL)){
-                vis[x1][y1]=true;
-  dfs(x1,y1,str+board[x1][y1],root->children[board[x1][y1]-'a'],vis,board);
-                vis[x1][y1]=false;
+
+        for(int i1=0;i1<4;i1++){
+            int x1=i+dx[i1];
+            int y1=j+dy[i1];
+            if(x1>=0 && x1<board.size() && y1>=0 && y1<board[0].size() && vis[x1][y1]==false){
+                find(x1,y1,str,board,vis,temp);
             }
         }
+        
+        vis[i][j]=false;
+        str.pop_back();
+            
     }
 };
