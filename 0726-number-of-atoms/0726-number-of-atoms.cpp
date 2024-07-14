2b399c912fd34c32d86c77e906ddecebaf0850f2
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2024-07-14 08:35:11 +0530

    Time: 3 ms (46.43%), Space: 10.3 MB (17.86%) - LeetHub

---
+++ 0726-number-of-atoms/0726-number-of-atoms.cpp
@@ -0,0 +1,103 @@
+class Solution {
+public:
+    string countOfAtoms(string form) {
+       
+        
+        int n=form.size();
+         
+        stack<pair<string,int>>st;
+        
+        int i=0;
+        
+        while(i<n){
+            
+            
+            if(form[i]=='('){
+                st.push({"(",0});
+                i++;
+            }
+            
+            else if(form[i]==')'){
+                
+                int no=0;
+                
+                i++;
+                
+                while(i<n && isdigit(form[i])){  // total bracket no
+                    no=no*10+(form[i]-'0');
+                    i++;
+                }
+                
+                if(no==0)
+                    no=1;
+                
+                unordered_map<string,int>mp1;
+                
+                while(st.top().first!="("){
+                    string str=st.top().first;
+                    int cnt=st.top().second;
+                    st.pop();
+                    mp1[str]+=cnt*no;
+                    
+                }
+                
+                st.pop();
+                
+                for(auto it:mp1){
+                  st.push({it.first,it.second});
+                }
+                
+            }
+            
+            else{
+              
+                string str;
+                
+                str.push_back(form[i]);
+                
+                i++;
+                
+    while(i<n && (form[i]!=')') && (form[i]!='(') && islower(form[i])){
+        
+                    str+=form[i];
+        
+                    i++;
+        
+                }
+                
+                int cnt=0;
+                
+                while(i<n && isdigit(form[i])){
+                    cnt=cnt*10+(form[i]-'0');
+                    i++;
+                }
+                                
+                if(cnt==0)
+                st.push({str,1});
+                else
+                    st.push({str,cnt});
+                
+            }
+        }
+        
+        map<string,int>mp;
+  
+        while(st.size()>0){
+            mp[st.top().first]+=st.top().second;
+            st.pop();
+        }
+        
+        
+        string ans;
+        
+        for(auto it:mp){
+            ans+=it.first;
+            if(it.second>1)
+            ans+=to_string(it.second);
+            
+        }
+        
+        return ans;
+        
+    }
+};
