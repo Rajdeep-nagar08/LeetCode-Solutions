0bdc2c2b3b77b07a2c84b1e355490a070934ceaa
Rajdeep Nagar <92351292+Rajdeep-nagar08@users.noreply.github.com>
2024-07-31 22:18:27 +0530

    Time: 61 ms (89.42%), Space: 28 MB (55.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,4 +1,6 @@
 #define lln long long int
+
+lln dp[201][10001];
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
@@ -8,29 +10,33 @@ public:
             sum += x;
         if (sum % 2)
             return false;
-        
-        vector<vector<int>> memo(n, vector<int>(sum / 2 + 1, -1));
-        return canPartitionRecursive(nums, sum / 2, 0, memo);
+            
+        memset(dp,-1,sizeof(dp));
+            
+        return find(0, nums,sum/2);
     }
     
 private:
-    bool canPartitionRecursive(vector<int>& nums, int target, int currentIndex, vector<vector<int>>& memo) {
-        if (target == 0)
+    bool find(int i,vector<int>&nums, int target) {
+       
+       if (target == 0)
             return true;
-        if (currentIndex >= nums.size())
+            
+        if (i>= nums.size())
             return false;
 
-        if (memo[currentIndex][target] != -1)
-            return memo[currentIndex][target];
+        if (dp[i][target] != -1)
+            return dp[i][target];
 
-        if (nums[currentIndex] <= target) {
-            if (canPartitionRecursive(nums, target - nums[currentIndex], currentIndex + 1, memo)) {
-                memo[currentIndex][target] = 1;
+        if (nums[i] <= target) {
+            if (find(i+1,nums,target-nums[i])) {
+                dp[i][target] = 1;
                 return true;
             }
         }
 
-        memo[currentIndex][target] = canPartitionRecursive(nums, target, currentIndex + 1, memo) ? 1 : 0;
-        return memo[currentIndex][target];
+        dp[i][target] = find(i+1,nums,target)?1:0;
+        
+        return dp[i][target];
     }
 };
